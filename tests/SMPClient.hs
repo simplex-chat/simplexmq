12c4b2ee2c0bdff09160b02a2f261e2e4f8a9358
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-01-11 08:07:07 +0000

    simplify smpServerTest

--- tests/SMPClient.hs
+++ tests/SMPClient.hs
@@ -54,10 +54,7 @@ runSmpTestN nClients test = withSmpServer $ run [] nClients
     run hs n = testSMPClient $ \h -> run (h : hs) (n - 1)
 
 smpServerTest :: RawTransmission -> IO RawTransmission
-smpServerTest command = runSmpTest (`sendReceive` command)
-  where
-    sendReceive :: Handle -> RawTransmission -> IO RawTransmission
-    sendReceive h t = tPutRaw h t >> tGetRaw h
+smpServerTest cmd = runSmpTest $ \h -> tPutRaw h cmd >> tGetRaw h
 
 smpTest :: (Handle -> IO ()) -> Expectation
 smpTest test' = runSmpTest test' `shouldReturn` ()
