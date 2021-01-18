65dd693c830b688b8943efb702a4ceeb3bd003ac
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-01-18 20:53:02 +0000

    parse agent messages with attoparsec (#18)
    * parse agent messages with attoparsec (WIP)
    
    * agent: refactor parsers
    
    * agent: parse commands and responses with attoparsec
    
    * refactor UTCTime parser
    
    * fix: updateRcvQueueStatus args
    
    * remove outdated comment
    
    * message parsing: PR feedback
    
    Co-authored-by: Efim Poberezkin <efim.poberezkin@gmail.com>

--- tests/SMPAgentClient.hs
+++ tests/SMPAgentClient.hs
@@ -57,7 +57,7 @@ withSmpAgent =
 
 testSMPAgentClient :: MonadUnliftIO m => (Handle -> m a) -> m a
 testSMPAgentClient client = do
-  threadDelay 100_000 -- TODO hack: thread delay for SMP agent to start
+  threadDelay 50_000 -- TODO hack: thread delay for SMP agent to start
   runTCPClient agentTestHost agentTestPort $ \h -> do
     line <- getLn h
     if line == "Welcome to SMP v0.2.0 agent"
