127bf991c5d6dc75205f8513f59fd79de0d6a50b
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-01-29 21:51:41 +0000

    chat prototype works

--- apps/dog-food/Main.hs
+++ apps/dog-food/Main.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -6,21 +8,21 @@
 module Main where
 
 import Control.Applicative ((<|>))
+import Control.Logger.Simple
 import Control.Monad.IO.Unlift
 import Control.Monad.Reader
 import Data.Attoparsec.ByteString.Char8 (Parser)
 import qualified Data.Attoparsec.ByteString.Char8 as A
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
-import Data.Functor
 import Numeric.Natural
 import Simplex.Messaging.Agent (getSMPAgentClient, runSMPAgentClient)
-import Simplex.Messaging.Agent.Client (AgentClient)
+import Simplex.Messaging.Agent.Client (AgentClient (..))
 import Simplex.Messaging.Agent.Env.SQLite
-import Simplex.Messaging.Agent.Transmission (SMPQueueInfo, serializeSmpQueueInfo, smpQueueInfoP)
+import Simplex.Messaging.Agent.Transmission
 import Simplex.Messaging.Client (smpDefaultConfig)
 import Simplex.Messaging.Transport (getLn, putLn)
-import Simplex.Messaging.Util (raceAny_)
+import Simplex.Messaging.Util (bshow, raceAny_)
 import System.IO
 import UnliftIO.Async
 import UnliftIO.STM
@@ -31,9 +33,12 @@ cfg =
     { tcpPort = undefined, -- TODO maybe take it out of config
       tbqSize = 16,
       connIdBytes = 12,
-      dbFile = "smp-agent.db",
+      dbFile = "dog-food.db",
       smpCfg = smpDefaultConfig
     }
+
+logCfg :: LogConfig
+logCfg = LogConfig {lc_file = Nothing, lc_stderr = True}
 
 data ChatClient = ChatClient
   { inQ :: TBQueue ChatCommand,
@@ -45,39 +50,50 @@ newtype Contact = Contact {toBs :: ByteString}
 -- | GroupMessage ChatGroup ByteString
 -- | AddToGroup Contact
 data ChatCommand
-  = InviteContact
-  | JoinInvitation SMPQueueInfo
+  = InviteContact Contact
+  | JoinInvitation Contact SMPQueueInfo
+  | TalkTo Contact
   | SendMessage Contact ByteString
 
 chatCommandP :: Parser ChatCommand
 chatCommandP =
-  "/invite" $> InviteContact
+  "/invite " *> inviteContact
     <|> "/join " *> joinInvitation
+    <|> "/talk " *> talkTo
     <|> "@" *> sendMessage
   where
-    joinInvitation :: Parser ChatCommand
-    joinInvitation = JoinInvitation <$> smpQueueInfoP
-    sendMessage = SendMessage <$> (Contact <$> A.takeTill (== ' ')) <* A.space <*> A.takeByteString
+    inviteContact = InviteContact . Contact <$> A.takeByteString
+    joinInvitation = JoinInvitation <$> contact <* A.space <*> smpQueueInfoP
+    talkTo = TalkTo . Contact <$> A.takeByteString
+    sendMessage = SendMessage <$> contact <* A.space <*> A.takeByteString
+    contact = Contact <$> A.takeTill (== ' ')
 
 data ChatResponse
   = Invitation SMPQueueInfo
   | Joined Contact
   | ReceivedMessage Contact ByteString
+  | Disconnected Contact
+  | YesYes
   | ErrorInput ByteString
+  | ChatError AgentErrorType
 
 serializeChatResponse :: ChatResponse -> ByteString
 serializeChatResponse = \case
-  Invitation qInfo -> "ask your contact to enter: /join " <> serializeSmpQueueInfo qInfo
+  Invitation qInfo -> "ask your contact to enter: /join <name> " <> serializeSmpQueueInfo qInfo
   Joined (Contact c) -> "@" <> c <> " connected"
   ReceivedMessage (Contact c) t -> "@" <> c <> ": " <> t
+  Disconnected (Contact c) -> "disconnected from @" <> c <> " - try sending \"/talk " <> c <> "\""
+  YesYes -> "on it!"
   ErrorInput t -> "invalid input: " <> t
+  ChatError e -> "chat error: " <> bshow e
 
 main :: IO ()
 main = do
   putStrLn "simplex chat"
-  -- setLogLevel LogInfo -- LogError
-  env <- newSMPAgentEnv cfg
-  runReaderT dogFoodChat env
+  setLogLevel LogInfo -- LogError
+  withGlobalLogging logCfg $ do
+    env <- newSMPAgentEnv cfg
+    runReaderT dogFoodChat env
 
 dogFoodChat :: (MonadUnliftIO m, MonadReader Env m) => m ()
 dogFoodChat = do
@@ -102,18 +118,38 @@ newChatClient qSize = do
   outQ <- newTBQueue qSize
   return ChatClient {inQ, outQ}
 
-dfSend :: MonadUnliftIO m => Handle -> ChatClient -> m ()
-dfSend h ChatClient {inQ, outQ} = liftIO (getLn h) >>= processOrError . A.parseOnly chatCommandP
+dfReceive :: MonadUnliftIO m => Handle -> ChatClient -> m ()
+dfReceive h ChatClient {inQ, outQ} =
+  forever $ liftIO (getLn h) >>= processOrError . A.parseOnly chatCommandP
   where
     processOrError = \case
       Right cmd -> atomically $ writeTBQueue inQ cmd
       Left err -> atomically . writeTBQueue outQ . ErrorInput $ B.pack err
 
-dfReceive :: MonadUnliftIO m => Handle -> ChatClient -> m ()
-dfReceive h ChatClient {outQ} = atomically (readTBQueue outQ) >>= liftIO . putLn h . serializeChatResponse
+dfSend :: MonadUnliftIO m => Handle -> ChatClient -> m ()
+dfSend h ChatClient {outQ} =
+  forever $ atomically (readTBQueue outQ) >>= liftIO . putLn h . serializeChatResponse
 
 dfClient :: MonadUnliftIO m => ChatClient -> AgentClient -> m ()
-dfClient _t _c = return ()
+dfClient t c =
+  forever . atomically $ readTBQueue (inQ t) >>= writeTBQueue (rcvQ c) . agentTransmission
+  where
+    agentTransmission :: ChatCommand -> ATransmission 'Client
+    agentTransmission = \case
+      InviteContact (Contact a) -> ("1", a, NEW (SMPServer "localhost" (Just "5223") Nothing))
+      JoinInvitation (Contact a) qInfo -> ("1", a, JOIN qInfo ReplyOn)
+      TalkTo (Contact a) -> ("1", a, SUB)
+      SendMessage (Contact a) msg -> ("1", a, SEND msg)
 
 dfSubscriber :: MonadUnliftIO m => ChatClient -> AgentClient -> m ()
-dfSubscriber _t _c = return ()
+dfSubscriber t c =
+  forever . atomically $ readTBQueue (sndQ c) >>= writeTBQueue (outQ t) . chatResponse
+  where
+    chatResponse :: ATransmission 'Agent -> ChatResponse
+    chatResponse (_, a, resp) = case resp of
+      INV qInfo -> Invitation qInfo
+      CON -> Joined (Contact a)
+      END -> Disconnected (Contact a)
+      MSG {m_body} -> ReceivedMessage (Contact a) m_body
+      OK -> YesYes
+      ERR e -> ChatError e
