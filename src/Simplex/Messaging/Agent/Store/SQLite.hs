c1659842229419cbc34f59f90d89eda61c756541
Efim Poberezkin <efim.poberezkin@gmail.com>
2021-02-04 14:08:43 +0400

    agent sqlite: rework schema, make methods transactional, enable foreign keys (#34)
    * agent sqlite: rework schema
    
    * agent sqlite: explicitly mark primary keys as NOT NULL
    
    * agent sqlite: adjust connections and queues fkeys
    
    * agent sqlite: remove ack_mode from queues tables
    
    * [WIP] agent sqlite: refactor methods
    
    * agent sqlite: implement transactional createRcvConn
    
    * add comment
    
    * agent sqlite: remove ConnAlias from createRcvConn signature
    
    * agent sqlite: implement transactional createSndConn
    
    * agent sqlite: remove monadic stack from store util methods
    
    * agent sqlite: refactor getConn
    
    * agent sqlite: rename conn -> dbConn
    
    * agent sqlite: move transactional logic to utils
    
    * agent sqlite: remove addServer from store interface
    
    * fix comment
    
    * agent sqlite: refactor getRcvQueue
    
    * agent sqlite: refactor deleteConn
    
    * agent sqlite: remove old deleteConn
    
    * agent sqlite: enable FKs
    
    * agent sqlite: refactor methods upgrading connection to duplex
    
    * agent sqlite: uncomment not implemented methods
    
    * agent sqlite: rename methods upgrading connection to duplex
    
    * use liftEither
    
    * agent sqlite: refactor update queue status methods
    
    * agent sqlite: refactor createMsg
    
    * clean up
    
    * fix compilation errors in src
    
    * fix existing tests
    
    * clean up tests
    
    * agent sqlite: test that foreign keys are enabled
    
    * change private members naming
    
    * tests: expect specific error code
    
    * clean up
    
    * agent sqlite: consistently separate lifts from logic to their own lines

--- src/Simplex/Messaging/Agent/Store/SQLite.hs
+++ src/Simplex/Messaging/Agent/Store/SQLite.hs
@@ -1,157 +1,106 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Simplex.Messaging.Agent.Store.SQLite where
+module Simplex.Messaging.Agent.Store.SQLite
+  ( SQLiteStore (..),
+    newSQLiteStore,
+  )
+where
 
-import Control.Monad
 import Control.Monad.Except
-import Control.Monad.IO.Unlift
-import Data.Maybe
+import Control.Monad.IO.Unlift (MonadUnliftIO)
 import qualified Database.SQLite.Simple as DB
 import Simplex.Messaging.Agent.Store
-import Simplex.Messaging.Agent.Store.SQLite.Schema
+import Simplex.Messaging.Agent.Store.SQLite.Schema (createSchema)
 import Simplex.Messaging.Agent.Store.SQLite.Util
 import Simplex.Messaging.Agent.Store.Types
 import Simplex.Messaging.Agent.Transmission
 import qualified Simplex.Messaging.Protocol as SMP
-import UnliftIO.STM
+
+data SQLiteStore = SQLiteStore
+  { dbFilename :: String,
+    dbConn :: DB.Connection
+  }
 
 newSQLiteStore :: MonadUnliftIO m => String -> m SQLiteStore
 newSQLiteStore dbFilename = do
-  conn <- liftIO $ DB.open dbFilename
-  liftIO $ createSchema conn
-  serversLock <- newTMVarIO ()
-  rcvQueuesLock <- newTMVarIO ()
-  sndQueuesLock <- newTMVarIO ()
-  connectionsLock <- newTMVarIO ()
-  messagesLock <- newTMVarIO ()
-  return
-    SQLiteStore
-      { dbFilename,
-        conn,
-        serversLock,
-        rcvQueuesLock,
-        sndQueuesLock,
-        connectionsLock,
-        messagesLock
-      }
+  dbConn <- liftIO $ DB.open dbFilename
+  liftIO $ createSchema dbConn
+  return SQLiteStore {dbFilename, dbConn}
 
 instance (MonadUnliftIO m, MonadError StoreError m) => MonadAgentStore SQLiteStore m where
-  addServer store smpServer = upsertServer store smpServer
+  createRcvConn :: SQLiteStore -> ReceiveQueue -> m ()
+  createRcvConn SQLiteStore {dbConn} rcvQueue =
+    liftIO $
+      createRcvQueueAndConn dbConn rcvQueue
 
-  createRcvConn :: SQLiteStore -> ConnAlias -> ReceiveQueue -> m ()
-  createRcvConn st connAlias rcvQueue = do
-    -- TODO test for duplicate connAlias
-    srvId <- upsertServer st (server (rcvQueue :: ReceiveQueue))
-    rcvQId <- insertRcvQueue st srvId rcvQueue
-    insertRcvConnection st connAlias rcvQId
+  createSndConn :: SQLiteStore -> SendQueue -> m ()
+  createSndConn SQLiteStore {dbConn} sndQueue =
+    liftIO $
+      createSndQueueAndConn dbConn sndQueue
 
-  createSndConn :: SQLiteStore -> ConnAlias -> SendQueue -> m ()
-  createSndConn st connAlias sndQueue = do
-    -- TODO test for duplicate connAlias
-    srvId <- upsertServer st (server (sndQueue :: SendQueue))
-    sndQ <- insertSndQueue st srvId sndQueue
-    insertSndConnection st connAlias sndQ
-
-  -- TODO refactor ito a single query with join, and parse as `Only connAlias :. rcvQueue :. sndQueue`
   getConn :: SQLiteStore -> ConnAlias -> m SomeConn
-  getConn st connAlias =
-    getConnection st connAlias >>= \case
-      (Just rcvQId, Just sndQId) -> do
-        rcvQ <- getRcvQueue st rcvQId
-        sndQ <- getSndQueue st sndQId
-        return $ SomeConn SCDuplex (DuplexConnection connAlias rcvQ sndQ)
-      (Just rcvQId, _) -> do
-        rcvQ <- getRcvQueue st rcvQId
-        return $ SomeConn SCReceive (ReceiveConnection connAlias rcvQ)
-      (_, Just sndQId) -> do
-        sndQ <- getSndQueue st sndQId
-        return $ SomeConn SCSend (SendConnection connAlias sndQ)
+  getConn SQLiteStore {dbConn} connAlias = do
+    queues <-
+      liftIO $
+        retrieveConnQueues dbConn connAlias
+    case queues of
+      (Just rcvQ, Just sndQ) -> return $ SomeConn SCDuplex (DuplexConnection connAlias rcvQ sndQ)
+      (Just rcvQ, Nothing) -> return $ SomeConn SCReceive (ReceiveConnection connAlias rcvQ)
+      (Nothing, Just sndQ) -> return $ SomeConn SCSend (SendConnection connAlias sndQ)
       _ -> throwError SEBadConn
 
-  getReceiveQueue :: SQLiteStore -> SMPServer -> SMP.RecipientId -> m (ConnAlias, ReceiveQueue)
-  getReceiveQueue st SMPServer {host, port} recipientId = do
-    rcvQueue <- getRcvQueueByRecipientId st recipientId host port
-    connAlias <- getConnAliasByRcvQueue st recipientId
-    return (connAlias, rcvQueue)
+  getRcvQueue :: SQLiteStore -> SMPServer -> SMP.RecipientId -> m ReceiveQueue
+  getRcvQueue SQLiteStore {dbConn} SMPServer {host, port} rcvId = do
+    rcvQueue <-
+      liftIO $
+        retrieveRcvQueue dbConn host port rcvId
+    case rcvQueue of
+      Just rcvQ -> return rcvQ
+      _ -> throwError SENotFound
 
-  -- TODO make transactional
-  addSndQueue :: SQLiteStore -> ConnAlias -> SendQueue -> m ()
-  addSndQueue st connAlias sndQueue =
-    getConn st connAlias
-      >>= \case
-        SomeConn SCDuplex _ -> throwError (SEBadConnType CDuplex)
-        SomeConn SCSend _ -> throwError (SEBadConnType CSend)
-        SomeConn SCReceive _ -> do
-          srvId <- upsertServer st (server (sndQueue :: SendQueue))
-          sndQ <- insertSndQueue st srvId sndQueue
-          updateRcvConnectionWithSndQueue st connAlias sndQ
-
-  -- TODO make transactional
-  addRcvQueue :: SQLiteStore -> ConnAlias -> ReceiveQueue -> m ()
-  addRcvQueue st connAlias rcvQueue =
-    getConn st connAlias
-      >>= \case
-        SomeConn SCDuplex _ -> throwError (SEBadConnType CDuplex)
-        SomeConn SCReceive _ -> throwError (SEBadConnType CReceive)
-        SomeConn SCSend _ -> do
-          srvId <- upsertServer st (server (rcvQueue :: ReceiveQueue))
-          rcvQ <- insertRcvQueue st srvId rcvQueue
-          updateSndConnectionWithRcvQueue st connAlias rcvQ
-
-  -- TODO think about design of one-to-one relationships between connections ans send/receive queues
-  -- - Make wide `connections` table? -> Leads to inability to constrain queue fields on SQL level
-  -- - Make bi-directional foreign keys deferred on queue side?
-  --   * Involves populating foreign keys on queues' tables and reworking store
-  --   * Enables cascade deletes
-  --   ? See https://sqlite.org/foreignkeys.html#fk_deferred
-  -- - Keep as is and just wrap in transaction?
   deleteConn :: SQLiteStore -> ConnAlias -> m ()
-  deleteConn st connAlias = do
-    (rcvQId, sndQId) <- getConnection st connAlias
-    forM_ rcvQId $ deleteRcvQueue st
-    forM_ sndQId $ deleteSndQueue st
-    deleteConnection st connAlias
-    when (isNothing rcvQId && isNothing sndQId) $ throwError SEBadConn
+  deleteConn SQLiteStore {dbConn} connAlias =
+    liftIO $
+      deleteConnCascade dbConn connAlias
+
+  upgradeRcvConnToDuplex :: SQLiteStore -> ConnAlias -> SendQueue -> m ()
+  upgradeRcvConnToDuplex SQLiteStore {dbConn} connAlias sndQueue =
+    liftIO
+      (updateRcvConnWithSndQueue dbConn connAlias sndQueue)
+      >>= liftEither
+
+  upgradeSndConnToDuplex :: SQLiteStore -> ConnAlias -> ReceiveQueue -> m ()
+  upgradeSndConnToDuplex SQLiteStore {dbConn} connAlias rcvQueue =
+    liftIO
+      (updateSndConnWithRcvQueue dbConn connAlias rcvQueue)
+      >>= liftEither
 
   removeSndAuth :: SQLiteStore -> ConnAlias -> m ()
   removeSndAuth _st _connAlias = throwError SENotImplemented
 
-  -- TODO throw error if queue doesn't exist
-  updateRcvQueueStatus :: SQLiteStore -> ReceiveQueue -> QueueStatus -> m ()
-  updateRcvQueueStatus st ReceiveQueue {rcvId, server = SMPServer {host, port}} status =
-    updateReceiveQueueStatus st rcvId host port status
+  setRcvQueueStatus :: SQLiteStore -> ReceiveQueue -> QueueStatus -> m ()
+  setRcvQueueStatus SQLiteStore {dbConn} rcvQueue status =
+    liftIO $
+      updateRcvQueueStatus dbConn rcvQueue status
 
-  -- TODO throw error if queue doesn't exist
-  updateSndQueueStatus :: SQLiteStore -> SendQueue -> QueueStatus -> m ()
-  updateSndQueueStatus st SendQueue {sndId, server = SMPServer {host, port}} status =
-    updateSendQueueStatus st sndId host port status
+  setSndQueueStatus :: SQLiteStore -> SendQueue -> QueueStatus -> m ()
+  setSndQueueStatus SQLiteStore {dbConn} sndQueue status =
+    liftIO $
+      updateSndQueueStatus dbConn sndQueue status
 
-  -- TODO decrease duplication of queue direction checks?
   createMsg :: SQLiteStore -> ConnAlias -> QueueDirection -> AgentMsgId -> AMessage -> m ()
-  createMsg st connAlias qDirection agentMsgId msg = do
-    case qDirection of
-      RCV -> do
-        (rcvQId, _) <- getConnection st connAlias
-        case rcvQId of
-          Just _ -> insertMsg st connAlias qDirection agentMsgId $ serializeAgentMessage msg
-          Nothing -> throwError SEBadQueueDirection
-      SND -> do
-        (_, sndQId) <- getConnection st connAlias
-        case sndQId of
-          Just _ -> insertMsg st connAlias qDirection agentMsgId $ serializeAgentMessage msg
-          Nothing -> throwError SEBadQueueDirection
+  createMsg SQLiteStore {dbConn} connAlias qDirection agentMsgId aMsg =
+    liftIO
+      (insertMsg dbConn connAlias agentMsgId aMsg)
+      >>= liftEither
+    where
+      insertMsg = case qDirection of
+        RCV -> insertRcvMsg
+        SND -> insertSndMsg
 
   getLastMsg :: SQLiteStore -> ConnAlias -> QueueDirection -> m MessageDelivery
   getLastMsg _st _connAlias _dir = throwError SENotImplemented
@@ -159,7 +108,7 @@ instance (MonadUnliftIO m, MonadError StoreError m) => MonadAgentStore SQLiteSto
   getMsg :: SQLiteStore -> ConnAlias -> QueueDirection -> AgentMsgId -> m MessageDelivery
   getMsg _st _connAlias _dir _msgId = throwError SENotImplemented
 
-  -- TODO missing status parameter?
+  -- ? missing status parameter?
   updateMsgStatus :: SQLiteStore -> ConnAlias -> QueueDirection -> AgentMsgId -> m ()
   updateMsgStatus _st _connAlias _dir _msgId = throwError SENotImplemented
 
