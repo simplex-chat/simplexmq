0ff3fda5df14ddbef9612933564390773e9578cf
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-02-13 10:55:55 +0000

    remove extension

--- src/Simplex/Messaging/Crypto.hs
+++ src/Simplex/Messaging/Crypto.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
 module Simplex.Messaging.Crypto where
@@ -49,7 +48,7 @@ pssParams :: PSS.PSSParams SHA256 ByteString ByteString
 pssParams = PSS.defaultPSSParams SHA256
 
 sign :: MonadRandom m => PrivateKey -> ByteString -> m (Either C.Error Signature)
-sign k msg = Signature <$$> PSS.signSafer pssParams (rsaPrivateKey k) msg
+sign pk msg = Signature <$$> PSS.signSafer pssParams (rsaPrivateKey pk) msg
 
 verify :: PublicKey -> Signature -> ByteString -> Maybe Verified
 verify k (Signature sig) msg =
@@ -58,15 +57,15 @@ verify k (Signature sig) msg =
     else Nothing
 
 rsaPrivateKey :: PrivateKey -> C.PrivateKey
-rsaPrivateKey PrivateKey {private_size, private_n, private_d} =
+rsaPrivateKey pk =
   C.PrivateKey
     { C.private_pub =
         PublicKey
-          { public_size = private_size,
-            public_n = private_n,
+          { public_size = private_size pk,
+            public_n = private_n pk,
             public_e = undefined
           },
-      C.private_d = private_d,
+      C.private_d = private_d pk,
       C.private_p = undefined,
       C.private_q = undefined,
       C.private_dP = undefined,
