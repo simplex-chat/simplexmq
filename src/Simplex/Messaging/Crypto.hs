8eed581f6fc0c646c879481d4e50375f6454d609
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-02-14 10:59:48 +0000

    rename RSA module name to R to avoid confusion with C used for S.M.Crypto

--- src/Simplex/Messaging/Crypto.hs
+++ src/Simplex/Messaging/Crypto.hs
@@ -21,7 +21,7 @@ import Crypto.Hash.Algorithms (SHA256 (..))
 import Crypto.Number.Generate (generateMax)
 import Crypto.Number.Prime (findPrimeFrom)
 import Crypto.Number.Serialize (i2osp, os2ip)
-import qualified Crypto.PubKey.RSA as C
+import qualified Crypto.PubKey.RSA as R
 import qualified Crypto.PubKey.RSA.PSS as PSS
 import Data.Attoparsec.ByteString.Char8 (Parser)
 import qualified Data.Attoparsec.ByteString.Char8 as A
@@ -36,7 +36,7 @@ import Database.SQLite.Simple.ToField (ToField (..))
 import Simplex.Messaging.Parsers (base64P)
 import Simplex.Messaging.Util (bshow, (<$$>))
 
-newtype PublicKey = PublicKey C.PublicKey deriving (Eq, Show)
+newtype PublicKey = PublicKey R.PublicKey deriving (Eq, Show)
 
 data PrivateKey = PrivateKey
   { private_size :: Int,
@@ -79,29 +79,29 @@ generateKeyPair :: Int -> IO KeyPair
 generateKeyPair size = loop
   where
     loop = do
-      (pub, priv) <- C.generate size =<< publicExponent
-      let n = C.public_n pub
-          d = C.private_d priv
+      (pub, priv) <- R.generate size =<< publicExponent
+      let n = R.public_n pub
+          d = R.private_d priv
        in if d * d < n
             then loop
             else
               return
                 ( PublicKey pub,
-                  PrivateKey {private_size = C.public_size pub, private_n = n, private_d = d}
+                  PrivateKey {private_size = R.public_size pub, private_n = n, private_d = d}
                 )
     publicExponent = findPrimeFrom . (+ 3) <$> generateMax pubExpRange
 
 pssParams :: PSS.PSSParams SHA256 ByteString ByteString
 pssParams = PSS.defaultPSSParams SHA256
 
-sign :: PrivateKey -> ByteString -> IO (Either C.Error Signature)
+sign :: PrivateKey -> ByteString -> IO (Either R.Error Signature)
 sign pk msg = Signature <$$> PSS.signSafer pssParams (rsaPrivateKey pk) msg
 
 verify :: PublicKey -> Signature -> ByteString -> Bool
 verify (PublicKey k) (Signature sig) msg = PSS.verify pssParams k msg sig
 
 serializePubKey :: PublicKey -> ByteString
-serializePubKey (PublicKey k) = serializeKey_ (C.public_size k, C.public_n k, C.public_e k)
+serializePubKey (PublicKey k) = serializeKey_ (R.public_size k, R.public_n k, R.public_e k)
 
 serializePrivKey :: PrivateKey -> ByteString
 serializePrivKey pk = serializeKey_ (private_size pk, private_n pk, private_d pk)
@@ -114,7 +114,7 @@ serializeKey_ (size, n, ex) = bshow size <> "," <> encInt n <> "," <> encInt ex
 pubKeyP :: Parser PublicKey
 pubKeyP = do
   (public_size, public_n, public_e) <- keyParser_
-  return . PublicKey $ C.PublicKey {C.public_size, C.public_n, C.public_e}
+  return . PublicKey $ R.PublicKey {R.public_size, R.public_n, R.public_e}
 
 privKeyP :: Parser PrivateKey
 privKeyP = do
@@ -132,19 +132,19 @@ keyParser_ = (,,) <$> (A.decimal <* ",") <*> (intP <* ",") <*> intP
   where
     intP = os2ip <$> base64P
 
-rsaPrivateKey :: PrivateKey -> C.PrivateKey
+rsaPrivateKey :: PrivateKey -> R.PrivateKey
 rsaPrivateKey pk =
-  C.PrivateKey
-    { C.private_pub =
-        C.PublicKey
-          { C.public_size = private_size pk,
-            C.public_n = private_n pk,
-            C.public_e = undefined
+  R.PrivateKey
+    { R.private_pub =
+        R.PublicKey
+          { R.public_size = private_size pk,
+            R.public_n = private_n pk,
+            R.public_e = undefined
           },
-      C.private_d = private_d pk,
-      C.private_p = 0,
-      C.private_q = 0,
-      C.private_dP = undefined,
-      C.private_dQ = undefined,
-      C.private_qinv = undefined
+      R.private_d = private_d pk,
+      R.private_p = 0,
+      R.private_q = 0,
+      R.private_dP = undefined,
+      R.private_dQ = undefined,
+      R.private_qinv = undefined
     }
