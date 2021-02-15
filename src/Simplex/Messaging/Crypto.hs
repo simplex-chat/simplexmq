609ba7c042006b0139b7fc271158c9729813404e
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-02-15 08:48:05 +0000

    E2E message encryption (#38)
    * RSA OAEP functions
    
    * encrypt/decrypt using AES AEAD GSM
    
    * fix JOIN syntax test
    
    * encrypt/decrypt functions working
    
    * e2e encryption!
    
    * refactor monadic transitions
    
    * rename liftError' to liftEitherError
    
    * combine CryproFailable functions

--- src/Simplex/Messaging/Crypto.hs
+++ src/Simplex/Messaging/Crypto.hs
@@ -1,13 +1,21 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Simplex.Messaging.Crypto
   ( PrivateKey (..),
     PublicKey (..),
     Signature (..),
+    CryptoError (..),
     generateKeyPair,
     sign,
     verify,
+    encrypt,
+    decrypt,
     serializePrivKey,
     serializePubKey,
     parsePrivKey,
@@ -17,16 +25,28 @@ module Simplex.Messaging.Crypto
   )
 where
 
+import Control.Exception (Exception)
+import Control.Monad.Except
+import Control.Monad.Trans.Except
+import Crypto.Cipher.AES (AES256)
+import qualified Crypto.Cipher.Types as AES
+import qualified Crypto.Error as CE
 import Crypto.Hash.Algorithms (SHA256 (..))
 import Crypto.Number.Generate (generateMax)
 import Crypto.Number.Prime (findPrimeFrom)
 import Crypto.Number.Serialize (i2osp, os2ip)
 import qualified Crypto.PubKey.RSA as R
+import qualified Crypto.PubKey.RSA.OAEP as OAEP
 import qualified Crypto.PubKey.RSA.PSS as PSS
+import Crypto.Random (getRandomBytes)
 import Data.Attoparsec.ByteString.Char8 (Parser)
 import qualified Data.Attoparsec.ByteString.Char8 as A
+import Data.Bifunctor (first)
+import qualified Data.ByteArray as BA
 import Data.ByteString.Base64
 import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as B
+import Data.ByteString.Internal (c2w, w2c)
 import Data.String
 import Database.SQLite.Simple as DB
 import Database.SQLite.Simple.FromField
@@ -34,9 +54,9 @@ import Database.SQLite.Simple.Internal (Field (..))
 import Database.SQLite.Simple.Ok (Ok (Ok))
 import Database.SQLite.Simple.ToField (ToField (..))
 import Simplex.Messaging.Parsers (base64P)
-import Simplex.Messaging.Util (bshow, (<$$>))
+import Simplex.Messaging.Util (bshow, liftEitherError, (<$$>))
 
-newtype PublicKey = PublicKey R.PublicKey deriving (Eq, Show)
+newtype PublicKey = PublicKey {rsaPublicKey :: R.PublicKey} deriving (Eq, Show)
 
 data PrivateKey = PrivateKey
   { private_size :: Int,
@@ -72,25 +92,94 @@ instance IsString Signature where
 
 newtype Verified = Verified ByteString deriving (Show)
 
+data CryptoError
+  = CryptoRSAError R.Error
+  | CryptoCipherError CE.CryptoError
+  | CryptoIVError
+  | CryptoDecryptError
+  deriving (Eq, Show, Exception)
+
 pubExpRange :: Integer
 pubExpRange = 2 ^ (1024 :: Int)
 
 generateKeyPair :: Int -> IO KeyPair
 generateKeyPair size = loop
   where
+    publicExponent = findPrimeFrom . (+ 3) <$> generateMax pubExpRange
+    privateKey s n d = PrivateKey {private_size = s, private_n = n, private_d = d}
     loop = do
       (pub, priv) <- R.generate size =<< publicExponent
-      let n = R.public_n pub
+      let s = R.public_size pub
+          n = R.public_n pub
           d = R.private_d priv
        in if d * d < n
             then loop
-            else
-              return
-                ( PublicKey pub,
-                  -- TODO add comments explaining why we throw away public key from private
-                  PrivateKey {private_size = R.public_size pub, private_n = n, private_d = d}
-                )
-    publicExponent = findPrimeFrom . (+ 3) <$> generateMax pubExpRange
+            else return (PublicKey pub, privateKey s n d)
+
+encrypt :: PublicKey -> ByteString -> ExceptT CryptoError IO ByteString
+encrypt k msg = do
+  aesKey <- randomBytes 32
+  ivBytes <- randomIVBytes @AES256
+  aead <- initAEAD @AES256 (aesKey, ivBytes)
+  let (authTag, msg') = encryptAES aead msg
+  encKeyIv <- encryptOAEP k (aesKey <> ivBytes)
+  return $ encKeyIv <> authTagToBS authTag <> msg'
+
+decrypt :: PrivateKey -> ByteString -> ExceptT CryptoError IO ByteString
+decrypt pk msg'' = do
+  let (encKeyIv, msg') = B.splitAt (private_size pk) msg''
+      (authTag, msg) = B.splitAt 16 msg'
+  keyIv <- B.splitAt 32 <$> decryptOAEP pk encKeyIv
+  aead <- initAEAD @AES256 keyIv
+  decryptAES aead msg (bsToAuthTag authTag)
+
+encryptAES :: AES.AEAD AES256 -> ByteString -> (AES.AuthTag, ByteString)
+encryptAES aead plaintext = AES.aeadSimpleEncrypt aead B.empty plaintext 16
+
+decryptAES :: AES.AEAD AES256 -> ByteString -> AES.AuthTag -> ExceptT CryptoError IO ByteString
+decryptAES aead ciphertext authTag =
+  maybeError CryptoDecryptError $ AES.aeadSimpleDecrypt aead B.empty ciphertext authTag
+
+initAEAD :: forall c. AES.BlockCipher c => (ByteString, ByteString) -> ExceptT CryptoError IO (AES.AEAD c)
+initAEAD (aesKey, ivBytes) = do
+  iv <- makeIV @c ivBytes
+  cryptoFailable $ do
+    cipher <- AES.cipherInit aesKey
+    AES.aeadInit AES.AEAD_GCM cipher iv
+
+randomIVBytes :: forall c. AES.BlockCipher c => ExceptT CryptoError IO ByteString
+randomIVBytes = randomBytes (AES.blockSize (undefined :: c))
+
+makeIV :: AES.BlockCipher c => ByteString -> ExceptT CryptoError IO (AES.IV c)
+makeIV bs = maybeError CryptoIVError $ AES.makeIV bs
+
+randomBytes :: Int -> ExceptT CryptoError IO ByteString
+randomBytes n = ExceptT $ Right <$> getRandomBytes n
+
+maybeError :: CryptoError -> Maybe a -> ExceptT CryptoError IO a
+maybeError e = maybe (throwE e) return
+
+authTagToBS :: AES.AuthTag -> ByteString
+authTagToBS = B.pack . map w2c . BA.unpack . AES.unAuthTag
+
+bsToAuthTag :: ByteString -> AES.AuthTag
+bsToAuthTag = AES.AuthTag . BA.pack . map c2w . B.unpack
+
+cryptoFailable :: CE.CryptoFailable a -> ExceptT CryptoError IO a
+cryptoFailable = liftEither . first CryptoCipherError . CE.eitherCryptoError
+
+oaepParams :: OAEP.OAEPParams SHA256 ByteString ByteString
+oaepParams = OAEP.defaultOAEPParams SHA256
+
+encryptOAEP :: PublicKey -> ByteString -> ExceptT CryptoError IO ByteString
+encryptOAEP (PublicKey k) aesKey =
+  liftEitherError CryptoRSAError $
+    OAEP.encrypt oaepParams k aesKey
+
+decryptOAEP :: PrivateKey -> ByteString -> ExceptT CryptoError IO ByteString
+decryptOAEP pk encKey =
+  liftEitherError CryptoRSAError $
+    OAEP.decryptSafer oaepParams (rsaPrivateKey pk) encKey
 
 pssParams :: PSS.PSSParams SHA256 ByteString ByteString
 pssParams = PSS.defaultPSSParams SHA256
