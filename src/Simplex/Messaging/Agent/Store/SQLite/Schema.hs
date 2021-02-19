6716e1bd140c6ed5a47bfda9f739738fd3c2eaa9
Efim Poberezkin <efim.poberezkin@gmail.com>
2021-02-19 20:51:59 +0400

    reiterate schema

--- src/Simplex/Messaging/Agent/Store/SQLite/Schema.hs
+++ src/Simplex/Messaging/Agent/Store/SQLite/Schema.hs
@@ -77,8 +77,8 @@ connections =
       snd_host TEXT,
       snd_port TEXT,
       snd_id BLOB,
-      last_rcv_msg_id INTEGER,
-      last_snd_msg_id INTEGER,
+      last_internal_msg_id INTEGER,
+      last_sending_msg_id INTEGER,
       PRIMARY KEY (conn_alias),
       FOREIGN KEY (rcv_host, rcv_port, rcv_id) REFERENCES rcv_queues (host, port, rcv_id),
       FOREIGN KEY (snd_host, snd_port, snd_id) REFERENCES snd_queues (host, port, snd_id)
@@ -89,64 +89,74 @@ messages :: Query
 messages =
   [sql|
     CREATE TABLE IF NOT EXISTS messages(
-      agent_msg_id INTEGER PRIMARY KEY,
-      agent_timestamp TEXT NOT NULL,
       conn_alias TEXT NOT NULL,
-      rcv_msg_id INTEGER,
-      snd_msg_id INTEGER,
-      message BLOB NOT NULL,
-      msg_status TEXT NOT NULL,
+      internal_id INTEGER NOT NULL,
+      internal_ts TEXT NOT NULL,
+      external_senders_id INTEGER,
+      external_sending_id INTEGER,
+      body BLOB NOT NULL,
+      status TEXT NOT NULL,
+      PRIMARY KEY (conn_alias, internal_id),
       FOREIGN KEY (conn_alias) REFERENCES connections (conn_alias),
-      FOREIGN KEY (conn_alias, rcv_msg_id) REFERENCES rcv_messages (conn_alias, rcv_msg_id),
-      FOREIGN KEY (conn_alias, snd_msg_id) REFERENCES snd_messages (conn_alias, snd_msg_id),
-    );
+      FOREIGN KEY (conn_alias, internal_id, external_senders_id)
+        REFERENCES rcv_messages (conn_alias, internal_id, senders_id)
+        DEFERRABLE INITIALLY DEFERRED,
+      FOREIGN KEY (conn_alias, internal_id, external_sending_id)
+        REFERENCES snd_messages (conn_alias, internal_id, sending_id)
+        DEFERRABLE INITIALLY DEFERRED
+    ) WITHOUT ROWID;
   |]
 
--- messages agent receives - sender_msg_id is id of the message at sender;
--- sender_msg_id corresponds to snd_msg_id from the sending side
+-- messages agent receives - senders_id is id of the message at sender,
+-- i.e. senders_id corresponds to sending_id from the sender's side
 -- TODO remove this comment
--- the order of insert into rcv_ and snd_messages tables (all in transaction):
--- 1. look up 'last_<rcv/snd>_msg_id' from 'connections' table;
--- 2. insert into 'messages' table - we get 'agent_msg_id' via autoincrement;
--- 3. insert into '<rcv/snd>_messages' table - it has deffered fk;
--- 4. update 'last_<rcv/snd>_msg_id' in 'connections' table - application is responsible for consistency.
+-- the order of insert into rcv_messages tables (all in transaction):
+-- 1. look up 'last_internal_msg_id' from 'connections' table;
+-- 2. insert into 'messages' table;
+-- 3. insert into 'rcv_messages' table;
+-- 4. update 'last_internal_msg_id' in 'connections' table - application is responsible for consistency.
 -- * investigate if it's possible to select and update in one query in sqlite
--- ? deferred fks are in assumption it's more convenient to delete from 'messages' side using agent_msg_id,
--- ? otherwise we could've just have unique constraints on 'messages' table and no foreign keys here
 rcvMessages :: Query
 rcvMessages =
   [sql|
     CREATE TABLE IF NOT EXISTS rcv_messages(
-      agent_msg_id INTEGER NOT NULL,
       conn_alias TEXT NOT NULL,
-      rcv_msg_id INTEGER NOT NULL,
-      broker_msg_id INTEGER NOT NULL,
-      broker_timestamp TEXT NOT NULL,
-      sender_msg_id INTEGER NOT NULL,
-      sender_timestamp TEXT NOT NULL,
-      PRIMARY KEY (conn_alias, rcv_msg_id),
-      FOREIGN KEY (agent_msg_id)
-        REFERENCES messages (agent_msg_id)
+      internal_id INTEGER NOT NULL,
+      senders_id INTEGER NOT NULL,
+      senders_ts TEXT NOT NULL,
+      broker_id INTEGER NOT NULL,
+      broker_ts TEXT NOT NULL,
+      PRIMARY KEY (conn_alias, internal_id, senders_id),
+      FOREIGN KEY (conn_alias, internal_id)
+        REFERENCES messages (conn_alias, internal_id)
         ON DELETE CASCADE
-        DEFERRABLE INITIALLY DEFERRED
-    );
+    ) WITHOUT ROWID;
   |]
+-- ? UNIQUE (conn_alias, senders_id)
+-- ? UNIQUE (conn_alias, broker_id)
 
--- messages agent sends - snd_msg_id is id of the message sent / to be sent,
+-- messages agent sends - sending_id is id of the message sent / to be sent,
 -- as in its number in order of sending
+-- TODO remove this comment
+-- the order of insert into snd_messages tables (all in transaction):
+-- 1. look up 'last_internal_msg_id' and 'last_sending_msg_id' from 'connections' table;
+-- 2. insert into 'messages' table;
+-- 3. insert into 'snd_messages' table;
+-- 4. update both 'last_..._msg_id' fields in 'connections' table - application is responsible for consistency.
 sndMessages :: Query
 sndMessages =
   [sql|
     CREATE TABLE IF NOT EXISTS snd_messages(
-      agent_msg_id INTEGER NOT NULL,
       conn_alias TEXT NOT NULL,
-      snd_msg_id INTEGER NOT NULL,
-      PRIMARY KEY (conn_alias, snd_msg_id),
-      FOREIGN KEY (agent_msg_id)
-        REFERENCES messages (agent_msg_id)
-        ON DELETE CASCADE
-        DEFERRABLE INITIALLY DEFERRED
-    );
+      internal_id INTEGER NOT NULL,
+      sending_id INTEGER NOT NULL,
+      sending_ts TEXT NOT NULL,
+      PRIMARY KEY (conn_alias, internal_id, sending_id),
+      FOREIGN KEY (conn_alias, internal_id)
+        REFERENCES messages (conn_alias, internal_id)
+        ON DELETE CASCADE,
+      UNIQUE (conn_alias, sending_id)
+    ) WITHOUT ROWID;
   |]
 
 createSchema :: Connection -> IO ()
