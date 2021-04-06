8041d55c1fda49c805d72846d3cbc82fbba17944
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-04-06 20:00:11 +0100

    support StyledString with System.Terminal

--- apps/dog-food/Styled.hs
+++ apps/dog-food/Styled.hs
@@ -1,13 +1,13 @@
-module Styled (StyledString (Styled), plain, bPlain, styledToANSITerm, styledToPlain) where
+module Styled (StyledString (..), plain, bPlain, styledToANSITerm, styledToPlain) where
 
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
 import Data.String
 import System.Console.ANSI (SGR (..), setSGRCode)
 
-data StyledString = Styled [SGR] String | Concat StyledString StyledString
+data StyledString = Styled [SGR] String | StyledString :<>: StyledString
 
-instance Semigroup StyledString where (<>) = Concat
+instance Semigroup StyledString where (<>) = (:<>:)
 
 instance Monoid StyledString where mempty = plain ""
 
@@ -23,8 +23,8 @@ bPlain = Styled [] . B.unpack
 styledToANSITerm :: StyledString -> String
 styledToANSITerm (Styled [] s) = s
 styledToANSITerm (Styled sgr s) = setSGRCode sgr <> s <> setSGRCode [Reset]
-styledToANSITerm (Concat s1 s2) = styledToANSITerm s1 <> styledToANSITerm s2
+styledToANSITerm (s1 :<>: s2) = styledToANSITerm s1 <> styledToANSITerm s2
 
 styledToPlain :: StyledString -> String
 styledToPlain (Styled _ s) = s
-styledToPlain (Concat s1 s2) = styledToPlain s1 <> styledToPlain s2
+styledToPlain (s1 :<>: s2) = styledToPlain s1 <> styledToPlain s2
--- apps/dog-food/Terminal.hs
+++ apps/dog-food/Terminal.hs
@@ -4,6 +4,7 @@ module Terminal where
 
 import Control.Monad.IO.Class (liftIO)
 import Styled
+import System.Console.ANSI.Types
 import System.Exit (exitSuccess)
 import System.Terminal as C
 
@@ -11,7 +12,48 @@ getLn :: IO String
 getLn = withTerminal (runTerminalT getTermLine)
 
 putLn :: StyledString -> IO ()
-putLn s = withTerminal . runTerminalT . putStringLn $ styledToPlain s
+putLn s =
+  withTerminal . runTerminalT $
+    putStyled s >> C.putLn >> flush
+
+putStyled :: MonadTerminal m => StyledString -> m ()
+putStyled (s1 :<>: s2) = putStyled s1 >> putStyled s2
+putStyled (Styled [] s) = putString s
+putStyled (Styled sgr s) = setSGR sgr >> putString s >> resetAttributes
+
+setSGR :: MonadTerminal m => [SGR] -> m ()
+setSGR = mapM_ $ \case
+  Reset -> resetAttributes
+  SetConsoleIntensity BoldIntensity -> setAttribute bold
+  SetConsoleIntensity _ -> resetAttribute bold
+  SetItalicized True -> setAttribute italic
+  SetItalicized _ -> resetAttribute italic
+  SetUnderlining NoUnderline -> resetAttribute underlined
+  SetUnderlining _ -> setAttribute underlined
+  SetSwapForegroundBackground True -> setAttribute inverted
+  SetSwapForegroundBackground _ -> resetAttribute inverted
+  SetColor l i c -> setAttribute . layer l . intensity i $ color c
+  SetBlinkSpeed _ -> pure ()
+  SetVisible _ -> pure ()
+  SetRGBColor _ _ -> pure ()
+  SetPaletteColor _ _ -> pure ()
+  SetDefaultColor _ -> pure ()
+  where
+    layer = \case
+      Foreground -> foreground
+      Background -> background
+    intensity = \case
+      Dull -> id
+      Vivid -> bright
+    color = \case
+      Black -> black
+      Red -> red
+      Green -> green
+      Yellow -> yellow
+      Blue -> blue
+      Magenta -> magenta
+      Cyan -> cyan
+      White -> white
 
 getTermLine :: MonadTerminal m => m String
 getTermLine = getChars ""
