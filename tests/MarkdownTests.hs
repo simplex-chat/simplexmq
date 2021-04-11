dbe1c4a781d15673f58cee85f51977a9a2fa1c4c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-04-11 09:54:14 +0100

    Secret markdown (#91)
    * secret text markdown
    
    * refactor

--- tests/MarkdownTests.hs
+++ tests/MarkdownTests.hs
@@ -11,6 +11,7 @@ import Test.Hspec
 markdownTests :: Spec
 markdownTests = do
   textFormat
+  secretText
   textColor
 
 textFormat :: Spec
@@ -48,6 +49,44 @@ textFormat = describe "text format (bold)" do
       `shouldBe` "this is " <> Markdown Bold "long _bold_ (not italic)" <> " " <> "text"
     parseMarkdown "snippet: `this is *bold text*`"
       `shouldBe` "snippet: " <> Markdown Snippet "this is *bold text*"
+
+secretText :: Spec
+secretText = describe "secret text" do
+  it "correct markdown" do
+    parseMarkdown "this is #black_secret# text"
+      `shouldBe` "this is " <> Markdown Secret "black_secret" <> " " <> "text"
+    parseMarkdown "##black_secret### text"
+      `shouldBe` Markdown Secret "#black_secret##" <> " " <> "text"
+    parseMarkdown "this is #black secret# text"
+      `shouldBe` "this is " <> Markdown Secret "black secret" <> " " <> "text"
+    parseMarkdown "##black secret### text"
+      `shouldBe` Markdown Secret "#black secret##" <> " " <> "text"
+    parseMarkdown "this is #secret#"
+      `shouldBe` "this is " <> Markdown Secret "secret"
+    parseMarkdown " #secret# text"
+      `shouldBe` " " <> Markdown Secret "secret" <> " " <> "text"
+    parseMarkdown "   #secret# text"
+      `shouldBe` "   " <> Markdown Secret "secret" <> " " <> "text"
+    parseMarkdown "this is #secret# "
+      `shouldBe` "this is " <> Markdown Secret "secret" <> " "
+    parseMarkdown "this is #secret#   "
+      `shouldBe` "this is " <> Markdown Secret "secret" <> "   "
+  it "ignored as markdown" do
+    parseMarkdown "this is # unformatted # text"
+      `shouldBe` "this is " <> "# unformatted #" <> " " <> "text"
+    parseMarkdown "this is #unformatted # text"
+      `shouldBe` "this is " <> "#unformatted #" <> " " <> "text"
+    parseMarkdown "this is # unformatted# text"
+      `shouldBe` "this is " <> "# unformatted#" <> " " <> "text"
+    parseMarkdown "this is ## unformatted ## text"
+      `shouldBe` "this is " <> "## unformatted ##" <> " " <> "text"
+    parseMarkdown "this is#unformatted# text"
+      `shouldBe` "this is#unformatted# text"
+    parseMarkdown "this is #unformatted text"
+      `shouldBe` "this is " <> "#unformatted text"
+  it "ignored internal markdown" do
+    parseMarkdown "snippet: `this is #secret_text#`"
+      `shouldBe` "snippet: " <> Markdown Snippet "this is #secret_text#"
 
 red :: Text -> Markdown
 red = Markdown (Colored Red)
