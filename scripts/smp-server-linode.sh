b1e0736686505e349c7593e0e357e1bb5a7cbbc2
Nikita Poberezkin <39944650+npoberezkin@users.noreply.github.com>
2021-06-01 21:50:12 +0300

    fix welcome message in linode script

--- scripts/smp-server-linode.sh
+++ scripts/smp-server-linode.sh
@@ -58,16 +58,16 @@ cat <<EOT >> $on_login_script
 #!/bin/bash
 # receives pubkey_hash file location as the first parameter
 
-ip_address=$(hostname -I | awk '{print$1}')
-hash=$(cat $1)
+ip_address=\$(hostname -I | awk '{print\$1}')
+hash=\$(cat \$1)
 
 cat <<EOF
 ********************************************************************************
 
-SMP server address: $ip_address#$hash
+SMP server address: \$ip_address#\$hash
 Check SMP server status with: systemctl status smp-server
 
-To keep this server secure, the UFW firewall is enabled. $myip
+To keep this server secure, the UFW firewall is enabled.
 All ports are BLOCKED except 22 (SSH), 80 (HTTP), 5223 (SMP server).
 
 ********************************************************************************
