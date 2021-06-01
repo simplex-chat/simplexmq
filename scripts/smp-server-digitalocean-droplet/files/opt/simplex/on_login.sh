e23e2db5f3524b65f7d61079525bf28b4c52b3db
Nikita Poberezkin <39944650+npoberezkin@users.noreply.github.com>
2021-06-01 21:06:07 +0300

    create pubkey_hash after init

--- scripts/smp-server-digitalocean-droplet/files/opt/simplex/on_login.sh
+++ scripts/smp-server-digitalocean-droplet/files/opt/simplex/on_login.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# receives pubkey_hash file location as the first parameter
 
 ip_address=$(hostname -I | awk '{print$1}')
-hash=$(cat /etc/opt/simplex/simplex.conf | grep hash: | cut -f2 -d":" | xargs)
+hash=$(cat $1)
 
 cat <<EOF
 ********************************************************************************
@@ -9,11 +10,8 @@ cat <<EOF
 SMP server address: $ip_address#$hash
 Check SMP server status with: systemctl status smp-server
 
-To keep this Droplet secure, the UFW firewall is enabled. $myip
+To keep this server secure, the UFW firewall is enabled.
 All ports are BLOCKED except 22 (SSH), 80 (HTTP), 5223 (SMP server).
-
-Please read the instructions at
-(TODO create instruction at https://marketplace.digitalocean.com/apps/simplex-chat)
 
 ********************************************************************************
 To stop seeing this message delete line - bash /opt/simplex/on_login.sh - from /root/.bashrc
--- scripts/smp-server-digitalocean-droplet/scripts/03-init-start.sh
+++ scripts/smp-server-digitalocean-droplet/scripts/03-init-start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# add welcome script to .bashrc
-echo "bash /opt/simplex/on_login.sh" >> /root/.bashrc
+bin_dir="/opt/simplex/bin"
+conf_dir="/etc/opt/simplex"
+var_dir="/var/opt/simplex"
+mkdir -p $bin_dir
+mkdir -p $conf_dir
+mkdir -p $var_dir
 
 echo "downloading SMP server release $release_tag"
 curl -s https://api.github.com/repos/simplex-chat/simplexmq/releases/tags/{$release_tag} \
@@ -10,14 +14,13 @@ curl -s https://api.github.com/repos/simplex-chat/simplexmq/releases/tags/{$rele
 | wget -qi -
 
 echo "preparing for SMP server initiaization"
-mv smp-server-ubuntu-20_04-x86-64 /opt/simplex/bin/smp-server
-chmod +x /opt/simplex/bin/smp-server
+mv smp-server-ubuntu-20_04-x86-64 $bin_dir/smp-server
+chmod +x $bin_dir/smp-server
 
-# add /opt/simplex/bin to PATH
 cat <<EOT >> /etc/profile.d/simplex.sh
 #!/bin/bash
 
-export PATH="$PATH:/opt/simplex/bin"
+export PATH="$PATH:$bin_dir"
 
 EOT
 source /etc/profile.d/simplex.sh
@@ -29,7 +32,7 @@ Description=SMP server systemd service
 
 [Service]
 Type=simple
-ExecStart=/bin/sh -c "/opt/simplex/bin/smp-server start"
+ExecStart=/bin/sh -c "$bin_dir/smp-server start"
 
 [Install]
 WantedBy=multi-user.target
@@ -38,12 +41,12 @@ EOT
 chmod 644 /etc/systemd/system/smp-server.service
 
 echo "initializing SMP server"
-init_res="/etc/opt/simplex/simplex.conf"
-init_res_tmp="/etc/opt/simplex/simplex.tmp"
-smp-server init -l > $init_res
-tail -n +2 "$init_res" > "$init_res_tmp" && mv "$init_res_tmp" "$init_res"
+hash_file="$conf_dir/pubkey_hash"
+smp-server init -l | grep "transport key hash:" | cut -f2 -d":" | xargs > $hash_file
 # turn off websockets support
-sed -e '/websockets/s/^/# /g' -i /etc/opt/simplex/smp-server.ini
+sed -e '/websockets/s/^/# /g' -i $conf_dir/smp-server.ini
+# add welcome script to .bashrc
+echo "bash /opt/simplex/on_login.sh $hash_file" >> /root/.bashrc
 
 echo "starting SMP server"
 sudo systemctl enable smp-server
