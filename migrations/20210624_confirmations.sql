7e3bb1ac831ff56247690cb18a8eff1d7977fd19
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2021-07-03 19:13:46 +1000

    don't recreate table

--- migrations/20210624_confirmations.sql
+++ migrations/20210624_confirmations.sql
@@ -11,28 +11,3 @@ CREATE TABLE conn_confirmations (
     REFERENCES connections (conn_alias)
     ON DELETE CASCADE
 ) WITHOUT ROWID;
-
--- remove snd_key from rcv_queues
-CREATE TEMPORARY TABLE rcv_queues_backup(host,port,rcv_id,conn_alias,rcv_private_key,snd_id,decrypt_key,verify_key,status);
-INSERT INTO rcv_queues_backup SELECT host,port,rcv_id,conn_alias,rcv_private_key,snd_id,decrypt_key,verify_key,status FROM rcv_queues;
-DROP TABLE rcv_queues;
-CREATE TABLE rcv_queues(
-  host TEXT NOT NULL,
-  port TEXT NOT NULL,
-  rcv_id BLOB NOT NULL,
-  conn_alias BLOB NOT NULL,
-  rcv_private_key BLOB NOT NULL,
-  snd_id BLOB,
-  decrypt_key BLOB NOT NULL,
-  verify_key BLOB,
-  status TEXT NOT NULL,
-  PRIMARY KEY (host, port, rcv_id),
-  FOREIGN KEY (host, port) REFERENCES servers (host, port),
-  FOREIGN KEY (conn_alias)
-    REFERENCES connections (conn_alias)
-    ON DELETE CASCADE
-    DEFERRABLE INITIALLY DEFERRED,
-  UNIQUE (host, port, snd_id)
-) WITHOUT ROWID;
-INSERT INTO rcv_queues SELECT host,port,rcv_id,conn_alias,rcv_private_key,snd_id,decrypt_key,verify_key,status FROM rcv_queues_backup;
-DROP TABLE rcv_queues_backup;
