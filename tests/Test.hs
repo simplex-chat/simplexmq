3769ab213c74e5c8ca927e709b365f48ed714340
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2021-12-15 21:13:29 +0400

    enable WebSockets over TLS

--- tests/Test.hs
+++ tests/Test.hs
@@ -4,7 +4,7 @@ import AgentTests (agentTests)
 import ProtocolErrorTests
 import ServerTests
 import Simplex.Messaging.Transport (TLS, Transport (..))
--- import Simplex.Messaging.Transport.WebSockets (WS)
+import Simplex.Messaging.Transport.WebSockets (WS)
 import System.Directory (createDirectoryIfMissing, removeDirectoryRecursive)
 import Test.Hspec
 
@@ -14,6 +14,6 @@ main = do
   hspec $ do
     describe "Protocol errors" protocolErrorTests
     describe "SMP server via TLS 1.3" $ serverTests (transport @TLS)
-    -- describe "SMP server via WebSockets" $ serverTests (transport @WS)
+    describe "SMP server via WebSockets" $ serverTests (transport @WS)
     describe "SMP client agent" $ agentTests (transport @TLS)
   removeDirectoryRecursive "tests/tmp"
