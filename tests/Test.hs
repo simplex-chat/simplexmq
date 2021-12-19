0acd67cbf0c9913ffb5d7099e7fb7b4975eeb71c
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2021-12-19 12:46:20 +0400

    fix ws

--- tests/Test.hs
+++ tests/Test.hs
@@ -14,6 +14,6 @@ main = do
   hspec $ do
     describe "Protocol errors" protocolErrorTests
     describe "SMP server via TLS 1.3" $ serverTests (transport @TLS)
-    -- describe "SMP server via WebSockets" $ serverTests (transport @WS)
+    describe "SMP server via WebSockets" $ serverTests (transport @WS)
     describe "SMP client agent" $ agentTests (transport @TLS)
   removeDirectoryRecursive "tests/tmp"
