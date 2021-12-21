959a9d3fb75aaa690384df3616bf817c6518af60
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-12-21 17:43:40 +0000

    initialize ratchets

--- src/Simplex/Messaging/Crypto/Ratchet.hs
+++ src/Simplex/Messaging/Crypto/Ratchet.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Simplex.Messaging.Crypto.Ratchet where
 
@@ -12,8 +14,8 @@ import Data.Int (Int64)
 import Simplex.Messaging.Crypto
 
 data RatchetState a = RatchetState
-  { rcDHs :: KeyPair a,
-    rcDHr :: Maybe (PublicKey a), -- initialized as Nothing for the party receiving the first message
+  { rcDHRs :: KeyPair a,
+    rcDHRr :: Maybe (PublicKey a), -- initialized as Nothing for the party receiving the first message
     rcRK :: RatchetKey,
     rcCKs :: Maybe RatchetKey, -- initialized as Nothing for the party receiving the first message
     rcCKr :: Maybe RatchetKey, -- initialized as Nothing for both
@@ -29,41 +31,82 @@ data RatchetState a = RatchetState
 data ARatchetState
   = forall a.
     (AlgorithmI a, DhAlgorithm a) =>
-    ARatchetState (SAlgorithm a) (PublicKey a)
+    ARatchetState (SAlgorithm a) (RatchetState a)
 
 -- | Input key material for double ratchet HKDF functions
 newtype RatchetKey = RatchetKey ByteString
 
--- | Sending ratchet initialization, equivalent to RatchetInitAliceHE in double ratchet doc
-initSndRatchet' :: (AlgorithmI a, DhAlgorithm a) => PublicKey a -> PrivateKey a -> ByteString -> IO (RatchetState a)
-initSndRatchet' rcvPubKey sndPrivKey salt = do
-  pure RatchetState {}
-  where
-    shared = dh' rcvPubKey sndPrivKey
-    (sk, hkSnd, nhkRcv) = initHkdf salt shared
+-- | Sending ratchet initialization, equivalent to RatchetInitAliceHE in double ratchet spec
+--
+-- Please note that sndPrivKey is not stored, and its public part together with random salt
+-- is sent to the recipient.
+initSndRatchet' ::
+  forall a. (AlgorithmI a, DhAlgorithm a) => PublicKey a -> PrivateKey a -> ByteString -> IO (RatchetState a)
+initSndRatchet' rKey sPKey salt = do
+  rcDHRs@(_, pk) <- generateKeyPair' @a 0
+  let (sk, rcHKs', rcNHKr) = initKdf salt rKey sPKey
+      (rcRK, rcCKs', rcNHKs) = rootKdf sk rKey pk
+  pure
+    RatchetState
+      { rcDHRs,
+        rcDHRr = Just rKey,
+        rcRK,
+        rcCKs = Just rcCKs',
+        rcCKr = Nothing,
+        rcNs = 0,
+        rcNr = 0,
+        rcPN = 0,
+        rcHKs = Just rcHKs',
+        rcHKr = Nothing,
+        rcNHKs,
+        rcNHKr
+      }
 
-initHkdf :: AlgorithmI a => ByteString -> DhSecret a -> (ByteString, Key, Key)
-initHkdf salt shared =
-  let out = hkdf salt (dhSecretBytes shared) "InitSimpleXRatchet" 96
-      (sk, rest) = B.splitAt 32 out
-      (hk, nhk) = B.splitAt 32 rest
-   in (sk, Key hk, Key nhk)
+-- | Receiving ratchet initialization, equivalent to RatchetInitBobHE in double ratchet spec
+--
+-- Please note that the public part of rcDHRs was sent to the sender
+-- as part of the connection request and random salt was received from the sender.
+initRcvRatchet' ::
+  forall a. (AlgorithmI a, DhAlgorithm a) => PublicKey a -> KeyPair a -> ByteString -> IO (RatchetState a)
+initRcvRatchet' sKey rcDHRs@(_, pk) salt = do
+  let (sk, rcNHKr, rcNHKs) = initKdf salt sKey pk
+  pure
+    RatchetState
+      { rcDHRs,
+        rcDHRr = Nothing,
+        rcRK = sk,
+        rcCKs = Nothing,
+        rcCKr = Nothing,
+        rcNs = 0,
+        rcNr = 0,
+        rcPN = 0,
+        rcHKs = Nothing,
+        rcHKr = Nothing,
+        rcNHKs,
+        rcNHKr
+      }
+
+initKdf :: (AlgorithmI a, DhAlgorithm a) => ByteString -> PublicKey a -> PrivateKey a -> (RatchetKey, Key, Key)
+initKdf salt k pk =
+  let (sk, hk, nhk) = hkdf3 salt k pk "SimpleXInitRatchet"
+   in (RatchetKey sk, Key hk, Key nhk)
+
+rootKdf :: (AlgorithmI a, DhAlgorithm a) => RatchetKey -> PublicKey a -> PrivateKey a -> (RatchetKey, RatchetKey, Key)
+rootKdf (RatchetKey rk) k pk =
+  let (rk', ck, nhk) = hkdf3 rk k pk "SimpleXRootRatchet"
+   in (RatchetKey rk', RatchetKey ck, Key nhk)
+
+hkdf3 :: (AlgorithmI a, DhAlgorithm a) => ByteString -> PublicKey a -> PrivateKey a -> ByteString -> (ByteString, ByteString, ByteString)
+hkdf3 salt k pk info =
+  split3 $ hkdf salt (dhSecretBytes $ dh' k pk) info 96
 
 hkdf :: ByteString -> ByteString -> ByteString -> Int -> ByteString
 hkdf salt ikm info len =
   let prk = H.extract salt ikm :: H.PRK SHA512
    in H.expand prk info len
 
--- From
--- def RatchetInitAliceHE(state, SK, bob_dh_public_key, shared_hka, shared_nhkb):
---     state.DHRs = GENERATE_DH()
---     state.DHRr = bob_dh_public_key
---     state.RK, state.CKs, state.NHKs = KDF_RK_HE(SK, DH(state.DHRs, state.DHRr))
---     state.CKr = None
---     state.Ns = 0
---     state.Nr = 0
---     state.PN = 0
---     state.MKSKIPPED = {}
---     state.HKs = shared_hka
---     state.HKr = None
---     state.NHKr = shared_nhkb
+split3 :: ByteString -> (ByteString, ByteString, ByteString)
+split3 s =
+  let (s1, rest) = B.splitAt 32 s
+      (s2, s3) = B.splitAt 32 rest
+   in (s1, s2, s3)
