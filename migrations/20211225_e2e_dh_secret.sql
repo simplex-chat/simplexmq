8677c4ef59e830ab6e01757bdfd112642ff8c712
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-12-28 19:14:15 +0000

    merge migration with E2E DH keys

--- migrations/20211225_e2e_dh_secret.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE rcv_queues ADD e2e_priv_key BLOB;
-ALTER TABLE rcv_queues ADD e2e_snd_pub_key BLOB;
-ALTER TABLE rcv_queues ADD e2e_dh_secret BLOB;
-
-ALTER TABLE snd_queues ADD e2e_pub_key BLOB;
-ALTER TABLE snd_queues ADD e2e_dh_secret BLOB;
-
-ALTER TABLE conn_confirmations ADD e2e_snd_pub_key BLOB;
