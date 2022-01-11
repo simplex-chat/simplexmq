0bfac998a5a5fbfc16cb611032decda5e738281b
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-01-11 10:41:02 +0400

    update Linode and DigitalOcean server recipes (#274)

--- scripts/smp-server-digitalocean-droplet/scripts/03-init-start.sh
+++ scripts/smp-server-digitalocean-droplet/scripts/03-init-start.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 bin_dir="/opt/simplex/bin"
+binary="$bin_dir/smp-server"
 conf_dir="/etc/opt/simplex"
 
+# Download latest release
 mkdir -p $bin_dir
+curl -L -o $binary https://github.com/simplex-chat/simplexmq/releases/latest/download/smp-server-ubuntu-20_04-x86-64
+chmod +x $binary
+$binary --version
 
-echo "downloading the latest SMP server release"
-curl -s https://api.github.com/repos/simplex-chat/simplexmq/releases/latest > release.json
-jq '.assets[].browser_download_url | select(test("smp-server-ubuntu-20_04-x86-64"))' release.json \
-| tr -d \" \
-| wget -qi -
-
-release_version=$(jq '.tag_name' release.json | tr -d \")
-echo "downloaded SMP server $release_version"
-rm release.json
-
-echo "preparing for SMP server initiaization"
-mv smp-server-ubuntu-20_04-x86-64 $bin_dir/smp-server
-chmod +x $bin_dir/smp-server
-
+# Add to PATH
 cat <<EOT >> /etc/profile.d/simplex.sh
 #!/bin/bash
 
@@ -27,31 +19,32 @@ export PATH="$PATH:$bin_dir"
 EOT
 source /etc/profile.d/simplex.sh
 
-# prepare SMP server systemd service
+# Initialize server
+smp-server init -l
+
+# Turn off websockets support
+sed -e '/websockets/s/^/# /g' -i $conf_dir/smp-server.ini
+
+# Server fingerprint
+fingerprint=$(cat $conf_dir/fingerprint)
+
+# On login script
+echo "bash /opt/simplex/on_login.sh $fingerprint" >> /root/.bashrc
+
+# Create and start systemd service
 cat <<EOT >> /etc/systemd/system/smp-server.service
 [Unit]
 Description=SMP server systemd service
 
 [Service]
 Type=simple
-ExecStart=/bin/sh -c "$bin_dir/smp-server start"
+ExecStart=/bin/sh -c "$binary start"
 
 [Install]
 WantedBy=multi-user.target
 
 EOT
+
 chmod 644 /etc/systemd/system/smp-server.service
-
-echo "initializing SMP server"
-smp-server init -l
-# CA certificate (identity/offline) fingerprint
-hash_file="$conf_dir/fingerprint"
-# turn off websockets support
-sed -e '/websockets/s/^/# /g' -i $conf_dir/smp-server.ini
-
-# add welcome script to .bashrc
-echo "bash /opt/simplex/on_login.sh $hash_file" >> /root/.bashrc
-
-echo "starting SMP server"
 sudo systemctl enable smp-server
 sudo systemctl start smp-server
