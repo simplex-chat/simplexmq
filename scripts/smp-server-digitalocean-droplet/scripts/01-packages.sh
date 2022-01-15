cfc285a62746466ec6ce0bf2a0a5d8e967f13dbc
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-01-15 13:30:40 +0400

    startup script to download latest SMP server release for DigitalOcean droplet

--- scripts/smp-server-digitalocean-droplet/scripts/01-packages.sh
+++ scripts/smp-server-digitalocean-droplet/scripts/01-packages.sh
@@ -2,19 +2,19 @@
 
 # https://superuser.com/questions/1638779/automatic-yess-to-linux-update-upgrade
 # https://superuser.com/questions/1412054/non-interactive-apt-upgrade
-sudo DEBIAN_FRONTEND=noninteractive \
-  apt-get \
-  -o Dpkg::Options::=--force-confold \
-  -o Dpkg::Options::=--force-confdef \
-  -y --allow-downgrades --allow-remove-essential --allow-change-held-packages \
-  update
+# sudo DEBIAN_FRONTEND=noninteractive \
+#   apt-get \
+#   -o Dpkg::Options::=--force-confold \
+#   -o Dpkg::Options::=--force-confdef \
+#   -y --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+#   update
 
-sudo DEBIAN_FRONTEND=noninteractive \
-  apt-get \
-  -o Dpkg::Options::=--force-confold \
-  -o Dpkg::Options::=--force-confdef \
-  -y --allow-downgrades --allow-remove-essential --allow-change-held-packages \
-  dist-upgrade
+# sudo DEBIAN_FRONTEND=noninteractive \
+#   apt-get \
+#   -o Dpkg::Options::=--force-confold \
+#   -o Dpkg::Options::=--force-confdef \
+#   -y --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+#   dist-upgrade
 
 # TODO install unattended-upgrades; jq is not needed on DigitalOcean
 # sudo DEBIAN_FRONTEND=noninteractive \
--- scripts/smp-server-digitalocean-droplet/scripts/03-init-start.sh
+++ scripts/smp-server-digitalocean-droplet/scripts/03-init-start.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# Download latest release
-bin_dir="/opt/simplex/bin"
-binary="$bin_dir/smp-server"
-mkdir -p $bin_dir
-curl -L -o $binary https://github.com/simplex-chat/simplexmq/releases/latest/download/smp-server-ubuntu-20_04-x86-64
-chmod +x $binary
-
-# / Add to PATH
-cat <<EOT >> /etc/profile.d/simplex.sh
+# / Create startup script
+cat > /opt/simplex/smp_server_startup.sh << EOF
 #!/bin/bash
 
-export PATH="$PATH:$bin_dir"
+set -eu
 
-EOT
+# Download latest release
+bin_dir="/opt/simplex/bin"
+binary="\$bin_dir/smp-server"
+mkdir -p \$bin_dir
+curl -L -o \$binary https://github.com/simplex-chat/simplexmq/releases/latest/download/smp-server-ubuntu-20_04-x86-64
+chmod +x \$binary
+
+echo \$bin_dir
+
+# / Add to PATH
+cat > /etc/profile.d/simplex.sh << EOF2
+#!/bin/bash
+
+export PATH="\$PATH:\$bin_dir"
+
+EOF2
 # Add to PATH /
 
 # Source and test PATH
@@ -21,31 +29,59 @@ source /etc/profile.d/simplex.sh
 smp-server --version
 
 # Initialize server
-ip_address=$(curl ifconfig.me)
-smp-server init -l --ip @ip_address
+ip_address=\$(hostname -I | awk '{print \$1}')
+smp-server init -l --ip \$ip_address
 
 # Server fingerprint
-fingerprint=$(cat /etc/opt/simplex/fingerprint)
+fingerprint=\$(cat /etc/opt/simplex/fingerprint)
 
 # Set up welcome script
-echo "bash /opt/simplex/on_login.sh $fingerprint $ip_address" >> /root/.bashrc
+echo "bash /opt/simplex/on_login.sh \$fingerprint \$ip_address" >> /root/.bashrc
 
-# / Create systemd service
-cat <<EOT >> /etc/systemd/system/smp-server.service
+# / Create systemd service for SMP server
+cat > /etc/systemd/system/smp-server.service << EOF2
 [Unit]
 Description=SMP server systemd service
 
 [Service]
 Type=simple
-ExecStart=/bin/sh -c "$binary start"
+ExecStart=/bin/sh -c "\$binary start"
 
 [Install]
 WantedBy=multi-user.target
 
-EOT
-# Create systemd service /
+EOF2
+# Create systemd service for SMP server /
 
-# Start systemd service
+# Start systemd service for SMP server
 chmod 644 /etc/systemd/system/smp-server.service
 sudo systemctl enable smp-server
 sudo systemctl start smp-server
+
+# Cleanup
+sudo systemctl stop smp-server-startup
+
+EOF
+# Create startup script /
+
+# / Create systemd service for startup script
+cat > /etc/systemd/system/smp-server-startup.service << EOF
+[Unit]
+Description=Startup script to download and initialize SMP server from latest release
+
+[Service]
+Type=simple
+ExecStart=/opt/simplex/smp_server_startup.sh
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+# Create systemd service for startup script /
+
+chmod +x /opt/simplex/smp_server_startup.sh
+
+# Start systemd service for startup script
+chmod 644 /etc/systemd/system/smp-server-startup.service
+sudo systemctl enable smp-server-startup
+sudo systemctl start smp-server-startup
