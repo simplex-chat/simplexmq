0374a3ab3bbf26b72729a945ee42dc116b5699d4
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2022-01-17 10:14:27 +0400

    1.0.2

--- src/Simplex/Messaging/Transport.hs
+++ src/Simplex/Messaging/Transport.hs
@@ -110,7 +110,7 @@ supportedSMPVersions :: VersionRange
 supportedSMPVersions = mkVersionRange 1 1
 
 simplexMQVersion :: String
-simplexMQVersion = "1.0.1"
+simplexMQVersion = "1.0.2"
 
 -- * Transport connection class
 
