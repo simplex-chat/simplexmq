bf020f3492be5dccea488acbe5f4db0196050cd3
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-03-20 18:00:16 +0000

    use CInt

--- src/Simplex/Messaging/Transport/KeepAlive.hs
+++ src/Simplex/Messaging/Transport/KeepAlive.hs
@@ -15,9 +15,9 @@ foreign import capi "netinet/tcp.h value TCP_KEEPINTVL" tcpKeepIntvl :: CInt
 foreign import capi "netinet/tcp.h value SOL_TCP" solTcp :: CInt
 
 data KeepAliveOpts = KeepAliveOpts
-  { keepCnt :: Int,
-    keepIdle :: Int,
-    keepIntvl :: Int
+  { keepCnt :: CInt,
+    keepIdle :: CInt,
+    keepIntvl :: CInt
   }
 
 defaultKeepAlive :: KeepAliveOpts
@@ -31,10 +31,10 @@ defaultKeepAlive =
 setSocketKeepAlive :: Socket -> KeepAliveOpts -> IO ()
 setSocketKeepAlive sock KeepAliveOpts {keepCnt, keepIdle, keepIntvl} = do
   setSocketOption sock KeepAlive 1
-  putStrLn $ "solTcp: " <> show solTcp
-  putStrLn $ "tcpKeepCnt: " <> show tcpKeepCnt
-  putStrLn $ "tcpKeepIdle: " <> show tcpKeepIdle
-  putStrLn $ "tcpKeepIntvl: " <> show tcpKeepIntvl
-  setSocketOption sock (SockOpt solTcp tcpKeepCnt) keepCnt
-  setSocketOption sock (SockOpt solTcp tcpKeepIdle) keepIdle
-  setSocketOption sock (SockOpt solTcp tcpKeepIntvl) keepIntvl
+  -- putStrLn $ "solTcp: " <> show solTcp
+  -- putStrLn $ "tcpKeepCnt: " <> show tcpKeepCnt
+  -- putStrLn $ "tcpKeepIdle: " <> show tcpKeepIdle
+  -- putStrLn $ "tcpKeepIntvl: " <> show tcpKeepIntvl
+  setSockOpt sock (SockOpt solTcp tcpKeepCnt) keepCnt
+  setSockOpt sock (SockOpt solTcp tcpKeepIdle) keepIdle
+  setSockOpt sock (SockOpt solTcp tcpKeepIntvl) keepIntvl
