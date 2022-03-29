62acbc4ad46f5b1c85b5ad0cced2170025b0d981
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-03-29 10:21:21 +0100

    PING error now throws error to restart SMPClient for more reliable re-connection (#342)

--- src/Simplex/Messaging/Client.hs
+++ src/Simplex/Messaging/Client.hs
@@ -114,7 +114,7 @@ smpDefaultConfig =
       defaultTransport = ("5223", transport @TLS),
       tcpTimeout = 4_000_000,
       tcpKeepAlive = Just defaultKeepAliveOpts,
-      smpPing = 600_000_000 -- 10min
+      smpPing = 300_000_000 -- 5 min
     }
 
 data Request = Request
@@ -194,7 +194,7 @@ getSMPClient smpServer cfg@SMPClientConfig {qSize, tcpTimeout, tcpKeepAlive, smp
     ping :: SMPClient -> IO ()
     ping c = forever $ do
       threadDelay smpPing
-      runExceptT $ sendSMPCommand c Nothing "" PING
+      void . either throwIO pure =<< runExceptT (sendSMPCommand c Nothing "" PING)
 
     process :: SMPClient -> IO ()
     process SMPClient {rcvQ, sentCommands} = forever $ do
