a2204c9b8440ac3a477ce33b4649fe6e321a4915
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-04-21 17:15:41 +0100

    remove comments, add EOL

--- src/Simplex/Messaging/Agent/Store.hs
+++ src/Simplex/Messaging/Agent/Store.hs
@@ -80,8 +80,7 @@ class Monad m => MonadAgentStore s m where
   -- Notification device token persistence
   createNtfToken :: s -> NtfToken -> m ()
 
-  -- TODO this should also return old tokens so that they are deleted from the server
-  getDeviceNtfToken :: s -> DeviceToken -> m (Maybe NtfToken, [NtfToken]) -- return current token if it exists
+  getDeviceNtfToken :: s -> DeviceToken -> m (Maybe NtfToken, [NtfToken])
   updateNtfTokenRegistration :: s -> NtfToken -> NtfTokenId -> C.DhSecretX25519 -> m ()
   updateNtfToken :: s -> NtfToken -> NtfTknStatus -> Maybe NtfTknAction -> m ()
   removeNtfToken :: s -> NtfToken -> m ()
