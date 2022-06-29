07bf2bca3b169b5e2c03e9d485e1623a8df2d2bd
JRoberts <8711996+jr-simplex@users.noreply.github.com>
2022-06-29 16:52:36 +0400

    v3.0.0-beta.0

--- src/Simplex/Messaging/Transport.hs
+++ src/Simplex/Messaging/Transport.hs
@@ -96,7 +96,7 @@ supportedSMPServerVRange :: VersionRange
 supportedSMPServerVRange = mkVersionRange 1 2
 
 simplexMQVersion :: String
-simplexMQVersion = "2.3.2"
+simplexMQVersion = "3.0.0-beta.0"
 
 -- * Transport connection class
 
