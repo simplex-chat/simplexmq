f0b233527fdd5cff577de832cd4bb5cd97dc33a3
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-09-29 18:27:16 +0100

    add import

--- src/Simplex/Messaging/Server/MsgStore/STM.hs
+++ src/Simplex/Messaging/Server/MsgStore/STM.hs
@@ -9,6 +9,7 @@
 
 module Simplex.Messaging.Server.MsgStore.STM where
 
+import Control.Concurrent.STM.TBQueue (flushTBQueue)
 import Control.Monad (when)
 import qualified Data.ByteString.Char8 as B
 import Data.Functor (($>))
