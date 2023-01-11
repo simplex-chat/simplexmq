2fd4ee74944690120e4a0adeeebec36ddf2067d0
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-01-11 12:37:08 +0000

    remove extra indices

--- src/Simplex/Messaging/Agent/Store/SQLite/Migrations/M20230110_users.hs
+++ src/Simplex/Messaging/Agent/Store/SQLite/Migrations/M20230110_users.hs
@@ -20,18 +20,6 @@ ALTER TABLE connections ADD COLUMN user_id INTEGER DEFAULT 1 CHECK (user_id NOT 
 REFERENCES users ON DELETE CASCADE;
 
 CREATE INDEX idx_connections_user ON connections(user_id);
-CREATE INDEX idx_rcv_queues_conn_id ON rcv_queues(conn_id);
-CREATE INDEX idx_snd_queues_conn_id ON snd_queues(conn_id);
-CREATE INDEX idx_messages_conn_id ON messages(conn_id);
-CREATE INDEX idx_messages_rcv_message ON messages(conn_id, internal_rcv_id);
-CREATE INDEX idx_messages_snd_message ON messages(conn_id, internal_snd_id);
-CREATE INDEX idx_rcv_messages_message ON rcv_messages(conn_id, internal_id);
-CREATE INDEX idx_snd_messages_message ON snd_messages(conn_id, internal_id);
-CREATE INDEX idx_conn_confirmations_conn_id on conn_confirmations(conn_id);
-CREATE INDEX idx_conn_invitations_conn_id on conn_invitations(contact_conn_id);
-CREATE INDEX idx_ratchets_conn_id on ratchets(conn_id);
-CREATE INDEX idx_skipped_messages_conn_id on skipped_messages(conn_id);
-CREATE INDEX idx_commands_conn_id on commands(conn_id);
 
 UPDATE connections SET user_id = 1;
 
--- src/Simplex/Messaging/Agent/Store/SQLite/Migrations/agent_schema.sql
+++ src/Simplex/Messaging/Agent/Store/SQLite/Migrations/agent_schema.sql
@@ -232,15 +232,3 @@ CREATE INDEX idx_snd_message_deliveries ON snd_message_deliveries(
 );
 CREATE TABLE users(user_id INTEGER PRIMARY KEY AUTOINCREMENT);
 CREATE INDEX idx_connections_user ON connections(user_id);
-CREATE INDEX idx_rcv_queues_conn_id ON rcv_queues(conn_id);
-CREATE INDEX idx_snd_queues_conn_id ON snd_queues(conn_id);
-CREATE INDEX idx_messages_conn_id ON messages(conn_id);
-CREATE INDEX idx_messages_rcv_message ON messages(conn_id, internal_rcv_id);
-CREATE INDEX idx_messages_snd_message ON messages(conn_id, internal_snd_id);
-CREATE INDEX idx_rcv_messages_message ON rcv_messages(conn_id, internal_id);
-CREATE INDEX idx_snd_messages_message ON snd_messages(conn_id, internal_id);
-CREATE INDEX idx_conn_confirmations_conn_id on conn_confirmations(conn_id);
-CREATE INDEX idx_conn_invitations_conn_id on conn_invitations(contact_conn_id);
-CREATE INDEX idx_ratchets_conn_id on ratchets(conn_id);
-CREATE INDEX idx_skipped_messages_conn_id on skipped_messages(conn_id);
-CREATE INDEX idx_commands_conn_id on commands(conn_id);
