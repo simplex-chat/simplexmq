a67bfcadac4407e00c68cb73a34eef4745baae89
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-03-03 22:54:52 +0000

    xftp: server types for any protocol

--- src/Simplex/FileTransfer/Client/Main.hs
+++ src/Simplex/FileTransfer/Client/Main.hs
@@ -40,6 +40,7 @@ import GHC.Records (HasField (getField))
 import Options.Applicative
 import Simplex.FileTransfer.Client
 import Simplex.FileTransfer.Client.Agent
+import Simplex.FileTransfer.Client.Presets
 import Simplex.FileTransfer.Description
 import Simplex.FileTransfer.Protocol
 import Simplex.FileTransfer.Transport (XFTPRcvChunkSpec (..))
@@ -133,17 +134,6 @@ data RandomFileOptions = RandomFileOptions
 
 defaultRetryCount :: Int
 defaultRetryCount = 3
-
-defaultXFTPServers :: NonEmpty XFTPServerWithAuth
-defaultXFTPServers =
-  L.fromList
-    [ "xftp://da1aH3nOT-9G8lV7bWamhxpDYdJ1xmW7j3JpGaDR5Ug=@xftp1.simplex.im",
-      "xftp://5vog2Imy1ExJB_7zDZrkV1KDWi96jYFyy9CL6fndBVw=@xftp2.simplex.im",
-      "xftp://PYa32DdYNFWi0uZZOprWQoQpIk5qyjRJ3EF7bVpbsn8=@xftp3.simplex.im",
-      "xftp://k_GgQl40UZVV0Y4BX9ZTyMVqX5ZewcLW0waQIl7AYDE=@xftp4.simplex.im",
-      "xftp://-bIo6o8wuVc4wpZkZD3tH-rCeYaeER_0lz1ffQcSJDs=@xftp5.simplex.im",
-      "xftp://6nSvtY9pJn6PXWTAIMNl95E1Kk1vD7FM2TeOA64CFLg=@xftp6.simplex.im"
-    ]
 
 cliCommandP :: Parser CliCommand
 cliCommandP =
