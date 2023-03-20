06f1ed9f86550ffc699141f33a33d6efd29cf6b1
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-03-20 13:15:20 +0000

    add migration

---
+++ src/Simplex/Messaging/Agent/Store/SQLite/Migrations/M20230320_retry_state.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Simplex.Messaging.Agent.Store.SQLite.Migrations.M20230320_retry_state where
+
+import Database.SQLite.Simple (Query)
+import Database.SQLite.Simple.QQ (sql)
+
+m20230320_retry_state :: Query
+m20230320_retry_state =
+  [sql|
+ALTER TABLE snd_messages ADD COLUMN retry_int_slow INTEGER NULL;
+ALTER TABLE snd_messages ADD COLUMN retry_int_fast INTEGER NULL;
+|]
