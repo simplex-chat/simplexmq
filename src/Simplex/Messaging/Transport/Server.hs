7f09aa77dccc990615969acbcef2d4f9483ba66c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-04-11 19:50:13 +0100

    3

--- src/Simplex/Messaging/Transport/Server.hs
+++ src/Simplex/Messaging/Transport/Server.hs
@@ -74,7 +74,7 @@ closeServer started clients sock = do
 startTCPServer :: TMVar Bool -> ServiceName -> IO Socket
 startTCPServer started port = withSocketsDo $ resolve >>= open >>= setStarted
   where
-    resolve = do
+    resolve =
       let hints = defaultHints {addrFlags = [AI_PASSIVE], addrSocketType = Stream}
        in select <$> getAddrInfo (Just hints) Nothing (Just port)
     select as = fromJust $ family AF_INET6 <|> family AF_INET
