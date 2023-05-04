c9e8b8875beb11358a6bcddb8a11942fdd4cac5c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-05-04 13:58:54 +0100

    style

--- src/Simplex/Messaging/Agent/Store/SQLite/Migrations.hs
+++ src/Simplex/Messaging/Agent/Store/SQLite/Migrations.hs
@@ -102,9 +102,7 @@ getCurrent db = map toMigration <$> DB.query_ db "SELECT name, down FROM migrati
 run :: Connection -> MigrationsToRun -> IO ()
 run db = \case
   MTRUp [] -> pure ()
-  MTRUp ms -> do
-    mapM_ runUp ms
-    execSQL "VACUUM;"
+  MTRUp ms -> mapM_ runUp ms >> execSQL "VACUUM;"
   MTRDown ms -> mapM_ runDown $ reverse ms
   MTRNone -> pure ()
   where
