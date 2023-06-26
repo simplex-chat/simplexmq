ac652787dbf33d50b9183e89373c3b24003a9d30
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-06-26 12:53:07 +0100

    5.1.3, ntf 1.4.2

--- src/Simplex/Messaging/Notifications/Server/Main.hs
+++ src/Simplex/Messaging/Notifications/Server/Main.hs
@@ -29,7 +29,7 @@ import System.IO (BufferMode (..), hSetBuffering, stderr, stdout)
 import Text.Read (readMaybe)
 
 ntfServerVersion :: String
-ntfServerVersion = "1.4.1"
+ntfServerVersion = "1.4.2"
 
 defaultSMPBatchDelay :: Int
 defaultSMPBatchDelay = 10000
