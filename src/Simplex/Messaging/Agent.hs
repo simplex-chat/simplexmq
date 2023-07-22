c54be32135ccd79e9bf2c82b30955c1e2fa8d822
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-07-22 09:16:06 +0100

    Revert "agent: expire messages on quota errors (#801)" (#807)
    This reverts commit 3fee468051f5b28ccfc588306684e5f7f1170ed5.

--- src/Simplex/Messaging/Agent.hs
+++ src/Simplex/Messaging/Agent.hs
@@ -1095,7 +1095,7 @@ runSmpQueueMsgDelivery c@AgentClient {subQ} cData@ConnData {userId, connId, dupl
                 SMP SMP.QUOTA -> case msgType of
                   AM_CONN_INFO -> connError msgId NOT_AVAILABLE
                   AM_CONN_INFO_REPLY -> connError msgId NOT_AVAILABLE
-                  _ -> retryIfNotExpired RISlow err
+                  _ -> retrySndMsg RISlow
                 SMP SMP.AUTH -> case msgType of
                   AM_CONN_INFO -> connError msgId NOT_AVAILABLE
                   AM_CONN_INFO_REPLY -> connError msgId NOT_AVAILABLE
@@ -1124,12 +1124,11 @@ runSmpQueueMsgDelivery c@AgentClient {subQ} cData@ConnData {userId, connId, dupl
                 _
                   -- for other operations BROKER HOST is treated as a permanent error (e.g., when connecting to the server),
                   -- the message sending would be retried
-                  | temporaryOrHostError e -> retryIfNotExpired RIFast err
+                  | temporaryOrHostError e -> do
+                    let timeoutSel = if msgType == AM_HELLO_ then helloTimeout else messageTimeout
+                    ifM (msgExpired timeoutSel) (notifyDel msgId err) (retrySndMsg RIFast)
                   | otherwise -> notifyDel msgId err
               where
-                retryIfNotExpired riMode err = do
-                  let timeoutSel = if msgType == AM_HELLO_ then helloTimeout else messageTimeout
-                  ifM (msgExpired timeoutSel) (notifyDel msgId err) (retrySndMsg riMode)
                 msgExpired timeoutSel = do
                   msgTimeout <- asks $ timeoutSel . config
                   currentTime <- liftIO getCurrentTime
