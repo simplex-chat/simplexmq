24cb53a7995432149fc85ff4ca79bbb0e92f1b7b
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-08-25 13:48:28 +0100

    allow empty env var values

--- apps/smp-server/Main.hs
+++ apps/smp-server/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import Control.Logger.Simple
@@ -17,7 +19,10 @@ logCfg = LogConfig {lc_file = Nothing, lc_stderr = True}
 main :: IO ()
 main = do
   setLogLevel LogDebug
-  cfgPath <- lookupEnv "SMP_SERVER_CFG_PATH"
-  logPath <- lookupEnv "SMP_SERVER_LOG_PATH"
+  cfgPath <- getEnvPath "SMP_SERVER_CFG_PATH" defaultCfgPath
+  logPath <- getEnvPath "SMP_SERVER_LOG_PATH" defaultLogPath
   withGlobalLogging logCfg $
-    smpServerCLI (fromMaybe defaultCfgPath cfgPath) (fromMaybe defaultLogPath logPath)
+    smpServerCLI cfgPath logPath
+    
+getEnvPath :: String -> FilePath -> FilePath
+getEnvPath name def = maybe def (\case "" -> def; f -> f) <$> lookupEnv name
