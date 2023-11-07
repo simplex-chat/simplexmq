1b61640b7714b691a49327fd01996f45193bf54d
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-11-07 21:40:09 +0000

    simplify

--- src/Simplex/Messaging/Crypto/SNTRUP761.hs
+++ src/Simplex/Messaging/Crypto/SNTRUP761.hs
@@ -15,30 +15,17 @@ import Simplex.Messaging.Crypto.SNTRUP761.Bindings
 -- Hybrid shared secret for crypto_box is defined as SHA256(DHSecret || KEMSharedKey),
 -- similar to https://datatracker.ietf.org/doc/draft-josefsson-ntruprime-hybrid/
 
-class KEMSharedSecret kem where kemSecretBytes :: kem -> ScrubbedBytes
-
 newtype KEMHybridSecret = KEMHybridSecret ScrubbedBytes
 
-newtype KEMHybridOrDHSecret = KEMHybridOrDHSecret ScrubbedBytes
-
-instance KEMSharedSecret KEMHybridSecret where kemSecretBytes (KEMHybridSecret secret) = secret
-
-instance KEMSharedSecret KEMHybridOrDHSecret where kemSecretBytes (KEMHybridOrDHSecret secret) = secret
-
 -- | NaCl @crypto_box@ decrypt with a shared hybrid DH + KEM secret and 192-bit nonce.
-kcbDecrypt :: KEMSharedSecret kem => kem -> CbNonce -> ByteString -> Either CryptoError ByteString
-kcbDecrypt = sbDecrypt_ . kemSecretBytes
+kcbDecrypt :: KEMHybridSecret -> CbNonce -> ByteString -> Either CryptoError ByteString
+kcbDecrypt (KEMHybridSecret k) = sbDecrypt_ k
 
 -- | NaCl @crypto_box@ encrypt with a shared hybrid DH + KEM secret and 192-bit nonce.
-kcbEncrypt :: KEMSharedSecret kem => kem -> CbNonce -> ByteString -> Int -> Either CryptoError ByteString
-kcbEncrypt = sbEncrypt_ . kemSecretBytes
+kcbEncrypt :: KEMHybridSecret -> CbNonce -> ByteString -> Int -> Either CryptoError ByteString
+kcbEncrypt (KEMHybridSecret k) = sbEncrypt_ k
 
 kemHybridSecret :: PublicKeyX25519 -> PrivateKeyX25519 -> KEMSharedKey -> KEMHybridSecret
 kemHybridSecret k pk (KEMSharedKey kem) =
   let DhSecretX25519 dh = C.dh' k pk
    in KEMHybridSecret $ BA.convert (hash $ BA.convert dh <> kem :: Digest SHA256)
-
-kemHybridOrDHSecret :: PublicKeyX25519 -> PrivateKeyX25519 -> Maybe KEMSharedKey -> KEMHybridOrDHSecret
-kemHybridOrDHSecret k pk = \case
-  Just kem -> KEMHybridOrDHSecret $ kemSecretBytes $ kemHybridSecret k pk kem
-  Nothing -> let DhSecretX25519 dh = C.dh' k pk in KEMHybridOrDHSecret $ BA.convert dh
