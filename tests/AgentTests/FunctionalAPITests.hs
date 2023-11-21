8ac97ed7004d1aba2c7e3cac14174409e1faddb4
IC Rainbow <aenor.realm@gmail.com>
2023-11-21 21:30:53 +0200

    add retry around removing tmp dir

--- tests/AgentTests/FunctionalAPITests.hs
+++ tests/AgentTests/FunctionalAPITests.hs
@@ -762,7 +762,6 @@ testDuplicateMessage t = do
       get bob2 =##> \case ("", c, Msg "hello 3") -> c == aliceId; _ -> False
   disconnectAgentClient alice2
   disconnectAgentClient bob2
-  threadDelay 1000000
 
 testSkippedMessages :: HasCallStack => ATransport -> IO ()
 testSkippedMessages t = do
@@ -1280,7 +1279,6 @@ testAsyncCommandsRestore t = do
       ("1", _, INV _) <- get alice'
       pure ()
   disconnectAgentClient alice'
-  threadDelay 1000000
 
 testAcceptContactAsync :: IO ()
 testAcceptContactAsync =
