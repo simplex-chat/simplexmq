0d0dd446a4099a08844c2a3be1689b360427eb23
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-11-26 13:12:01 +0000

    change xftp, ntf versions

--- src/Simplex/FileTransfer/Server/Main.hs
+++ src/Simplex/FileTransfer/Server/Main.hs
@@ -33,7 +33,7 @@ import System.IO (BufferMode (..), hSetBuffering, stderr, stdout)
 import Text.Read (readMaybe)
 
 xftpServerVersion :: String
-xftpServerVersion = "1.2.0"
+xftpServerVersion = "1.1.3"
 
 xftpServerCLI :: FilePath -> FilePath -> IO ()
 xftpServerCLI cfgPath logPath = do
