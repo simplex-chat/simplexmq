dccb51491d36f127ab9ed32c72240cbed97fb7a2
IC Rainbow <aenor.realm@gmail.com>
2023-11-29 16:54:37 +0200

    trace closeTLS exceptions

--- src/Simplex/Messaging/Transport.hs
+++ src/Simplex/Messaging/Transport.hs
@@ -73,6 +73,7 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Default (def)
 import Data.Functor (($>))
 import Data.Version (showVersion)
+import Debug.Trace (traceEventIO)
 import GHC.IO.Handle.Internals (ioe_EOF)
 import Network.Socket
 import qualified Network.TLS as T
@@ -82,7 +83,7 @@ import qualified Simplex.Messaging.Crypto as C
 import Simplex.Messaging.Encoding
 import Simplex.Messaging.Parsers (dropPrefix, parse, parseRead1, sumTypeJSON)
 import Simplex.Messaging.Transport.Buffer
-import Simplex.Messaging.Util (bshow, catchAll, catchAll_)
+import Simplex.Messaging.Util (bshow, catchAll)
 import Simplex.Messaging.Version
 import UnliftIO.Exception (Exception)
 import qualified UnliftIO.Exception as E
@@ -186,7 +187,7 @@ closeTLS :: T.Context -> IO ()
 closeTLS ctx =
   T.bye ctx -- sometimes socket was closed before 'TLS.bye' so we catch the 'Broken pipe' error here
     `E.finally` T.contextClose ctx
-    `catchAll_` pure ()
+    `catchAll` \(E.SomeException e) -> traceEventIO ("closeTLS/" <> takeWhile (/= ' ') (show e))
 
 supportedParameters :: T.Supported
 supportedParameters =
