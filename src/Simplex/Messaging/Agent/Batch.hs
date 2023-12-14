daa8d5461b65e449e404cce57b77b2aa821f91b2
IC Rainbow <aenor.realm@gmail.com>
2023-12-14 20:05:05 +0200

    add AgentEnvBatch for merging with external batches

--- src/Simplex/Messaging/Agent/Batch.hs
+++ src/Simplex/Messaging/Agent/Batch.hs
@@ -1,28 +1,30 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE LambdaCase #-}
 
 module Simplex.Messaging.Agent.Batch where
 
+import Control.Logger.Simple
 import Control.Monad (unless)
 import Control.Monad.Except (throwError, tryError)
+import Control.Monad.Reader (ReaderT)
 import Data.Traversable (for)
 import GHC.Stack (HasCallStack)
 import Simplex.Messaging.Agent.Client (AgentClient, withStore')
-import Simplex.Messaging.Agent.Env.SQLite (AgentMonad, AgentMonad')
+import Simplex.Messaging.Agent.Env.SQLite (AgentMonad, AgentMonad', Env)
 import Simplex.Messaging.Agent.Protocol (AgentErrorType)
 import qualified Simplex.Messaging.Agent.Store.SQLite.DB as DB
 import Simplex.Messaging.Batch
-import UnliftIO (atomically, newTVarIO, stateTVar)
-import Control.Logger.Simple
 import Simplex.Messaging.Util (tshow)
+import UnliftIO (atomically, newTVarIO, stateTVar)
 
 data AgentDB
 type instance BatchArgs AgentDB = DB.Connection
 type instance BatchError AgentDB = AgentErrorType
 type AgentBatch m = BatchVar AgentDB m
+type AgentEnvBatch m = AgentBatch (ReaderT Env m)
 
 execAgentBatch :: (AgentMonad m, HasCallStack) => AgentClient -> (AgentBatch m -> BatchT AgentErrorType m a) -> m a
 execAgentBatch c action = execAgentBatch' c action >>= either throwError pure
@@ -38,12 +40,13 @@ processAgentBatchAll :: AgentMonad' m => AgentClient -> AgentBatch m -> m ()
 processAgentBatchAll c agentBatch = do
   agents <- atomically $ stateTVar agentBatch (,[])
   unless (null agents) $ do
-    runEContT (processAgentBatch c agents) $ either (logError  . tshow) pure
+    runEContT (processAgentBatch c agents) $ either (logError . tshow) pure
     processAgentBatchAll c agentBatch
 
 processAgentBatch :: AgentMonad' m => AgentClient -> [BatchOperation AgentDB m] -> BatchT AgentErrorType m ()
 processAgentBatch c batch = do
-  actions <- liftEContError . withStore' c $ \db -> do -- XXX: may throw SEInternal when its `withTransaction` failed somehow.
+  actions <- liftEContError . withStore' c $ \db -> do
+    -- XXX: may throw SEInternal when its `withTransaction` failed somehow.
     -- XXX: perhaps that error should be broadcasted to all the actions depending on it (may trigger handler stampede)
     for batch $ \BatchOperation {step, next} -> do
       tryError (step db) >>= \case
