af93f28898b12778e9fa73cc2dffdeaf00f3e933
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2023-12-26 14:28:22 +0400

    add indexes

--- src/Simplex/Messaging/Agent/Store/SQLite/Migrations/M20231225_failed_work_items.hs
+++ src/Simplex/Messaging/Agent/Store/SQLite/Migrations/M20231225_failed_work_items.hs
@@ -15,11 +15,19 @@ ALTER TABLE ntf_subscriptions ADD COLUMN smp_failed INTEGER DEFAULT 0;
 ALTER TABLE rcv_files ADD COLUMN failed INTEGER DEFAULT 0;
 ALTER TABLE snd_files ADD COLUMN failed INTEGER DEFAULT 0;
 ALTER TABLE deleted_snd_chunk_replicas ADD COLUMN failed INTEGER DEFAULT 0;
+
+CREATE INDEX idx_rcv_files_status_created_at ON rcv_files(status, created_at);
+CREATE INDEX idx_snd_files_status_created_at ON snd_files(status, created_at);
+CREATE INDEX idx_snd_files_snd_file_entity_id ON snd_files(snd_file_entity_id);
 |]
 
 down_m20231225_failed_work_items :: Query
 down_m20231225_failed_work_items =
     [sql|
+DROP INDEX idx_rcv_files_status_created_at;
+DROP INDEX idx_snd_files_status_created_at;
+DROP INDEX idx_snd_files_snd_file_entity_id;
+
 ALTER TABLE snd_message_deliveries DROP COLUMN failed;
 ALTER TABLE commands DROP COLUMN failed;
 ALTER TABLE ntf_subscriptions DROP COLUMN ntf_failed;
