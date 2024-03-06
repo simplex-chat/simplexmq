00ae2cb6e134e3cd7c8089e30f95a9430d3c4e3d
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-03-06 23:16:43 +0000

    remove unused

--- src/Simplex/Messaging/Crypto/Ratchet.hs
+++ src/Simplex/Messaging/Crypto/Ratchet.hs
@@ -31,7 +31,6 @@ module Simplex.Messaging.Crypto.Ratchet
     PQSupport (..),
     pattern PQSupportOn,
     pattern PQSupportOff,
-    PQSupported (..),
     AUseKEM (..),
     RatchetKEMState (..),
     SRatchetKEMState (..),
@@ -339,11 +338,6 @@ instance StrEncoding AnyE2ERatchetParamsUri where
 type RcvE2ERatchetParams a = E2ERatchetParams 'RKSProposed a
 
 type SndE2ERatchetParams a = AE2ERatchetParams a
-
-class PQSupported a where pqSupported :: a -> PQSupport
-
-instance PQSupported (E2ERatchetParamsUri s a) where
-  pqSupported (E2ERatchetParamsUri _ _ _ kem_) = PQSupport $ isJust kem_
 
 data PrivRKEMParams (s :: RatchetKEMState) where
   PrivateRKParamsProposed :: KEMKeyPair -> PrivRKEMParams 'RKSProposed
