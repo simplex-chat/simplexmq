218fe86e25a77df351c1447b44f0e7b6b84b709b
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-08-20 13:22:55 +0400

    comment logs

--- src/Simplex/Messaging/Agent/Client.hs
+++ src/Simplex/Messaging/Agent/Client.hs
@@ -1058,11 +1058,12 @@ sendOrProxySMPCommand ::
   (SMPClient -> ExceptT SMPClientError IO ()) ->
   AM (Maybe SMPServer)
 sendOrProxySMPCommand c userId destSrv cmdStr senderId sendCmdViaProxy sendCmdDirectly = do
-  logDebug $ "MARKER AGNT sendOrProxySMPCommand"
+  -- logDebug $ "MARKER AGNT sendOrProxySMPCommand"
   sess <- mkTransportSession c userId destSrv senderId
-  shouldUsePrx <- shouldUseProxy
-  logDebug $ "MARKER AGNT sendOrProxySMPCommand after mkTransportSession, shouldUseProxy=" <> tshow shouldUsePrx
-  if shouldUsePrx then (sendViaProxy Nothing sess) else (sendDirectly sess $> Nothing)
+  -- shouldUsePrx <- shouldUseProxy
+  -- logDebug $ "MARKER AGNT sendOrProxySMPCommand after mkTransportSession, shouldUseProxy=" <> tshow shouldUsePrx
+  -- if shouldUsePrx then (sendViaProxy Nothing sess) else (sendDirectly sess $> Nothing)
+  ifM shouldUseProxy (sendViaProxy Nothing sess) (sendDirectly sess $> Nothing)
   where
     shouldUseProxy = do
       cfg <- getNetworkConfig c
@@ -1128,16 +1129,16 @@ sendOrProxySMPCommand c userId destSrv cmdStr senderId sendCmdViaProxy sendCmdDi
           | serverHostError e -> ifM directAllowed (sendDirectly destSess $> Nothing) (throwE e)
           | otherwise -> throwE e
     sendDirectly tSess = do
-      logDebug $ "MARKER AGNT sendOrProxySMPCommand sendDirectly"
+      -- logDebug $ "MARKER AGNT sendOrProxySMPCommand sendDirectly"
       withLogClient_ c tSess senderId ("SEND " <> cmdStr) $ \(SMPConnectedClient smp _) -> do
-        logDebug $ "MARKER AGNT sendOrProxySMPCommand sendDirectly in withLogClient_"
+        -- logDebug $ "MARKER AGNT sendOrProxySMPCommand sendDirectly in withLogClient_"
         r <- tryAgentError $ liftClient SMP (clientServer smp) $ sendCmdDirectly smp
         case r of
           Right () -> do
-            logDebug $ "MARKER AGNT sendOrProxySMPCommand sendDirectly Right"
+            -- logDebug $ "MARKER AGNT sendOrProxySMPCommand sendDirectly Right"
             atomically $ incSMPServerStat c userId destSrv sentDirect
           Left e -> do
-            logDebug $ "MARKER AGNT sendOrProxySMPCommand sendDirectly Left e=" <> tshow e
+            -- logDebug $ "MARKER AGNT sendOrProxySMPCommand sendDirectly Left e=" <> tshow e
             throwE e
 
 ipAddressProtected :: NetworkConfig -> ProtocolServer p -> Bool
