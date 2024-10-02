8f5f0f97d0aaeedafe822092ca46b1ef48410a62
Evgeny Poberezkin <evgeny@poberezkin.com>
2024-10-02 09:23:29 +0100

    random

--- tests/CoreTests/StoreLogTests.hs
+++ tests/CoreTests/StoreLogTests.hs
@@ -1,35 +1,45 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module CoreTests.StoreLogTests where
 
+import Control.Concurrent.STM
 import Control.Monad
+import Crypto.Random (ChaChaDRG)
 import qualified Data.ByteString.Char8 as B
 import Data.Either (partitionEithers)
 import qualified Data.Map.Strict as M
 import SMPClient
 import AgentTests.SQLiteTests
+import qualified Simplex.Messaging.Crypto as C
 import Simplex.Messaging.Encoding.String
 import Simplex.Messaging.Protocol
 import Simplex.Messaging.Server.StoreLog
 import Simplex.Messaging.Server.QueueStore
 import Test.Hspec
 
-testNewQueueRec :: Bool -> QueueRec
-testNewQueueRec sndSecure = QueueRec
-  { recipientId = EntityId "abcd",
-    recipientKey = testPublicAuthKey,
-    rcvDhSecret = testDhSecret,
-    senderId = EntityId "efgh",
-    senderKey = Just testPublicAuthKey,
-    sndSecure,
-    notifier = Nothing,
-    status = QueueActive,
-    updatedAt = Nothing
-  }
+testNewQueueRec :: TVar ChaChaDRG -> Bool -> IO QueueRec
+testNewQueueRec g sndSecure = do
+  recipientId <- atomically $ EntityId <$> C.randomBytes 24 g
+  senderId <- atomically $ EntityId <$> C.randomBytes 24 g
+  (recipientKey, _) <- atomically $ C.generateAuthKeyPair C.SX25519 g
+  (k, pk) <- atomically $ C.generateKeyPair @'C.X25519 g
+  pure QueueRec
+    { recipientId,
+      recipientKey,
+      rcvDhSecret = C.dh' k pk,
+      senderId,
+      senderKey = Nothing,
+      sndSecure,
+      notifier = Nothing,
+      status = QueueActive,
+      updatedAt = Nothing
+    }
 
 testNtfCreds :: NtfCreds
 testNtfCreds = NtfCreds
@@ -50,25 +60,28 @@ deriving instance Eq NtfCreds
 
 storeLogTests :: Spec
 storeLogTests =
-  forM_ [False, True] $ \sndSecure ->
+  forM_ [False, True] $ \sndSecure -> do
+    qr <- runIO $ do
+      g <- C.newRandom
+      testNewQueueRec g sndSecure
     testSMPStoreLog ("SMP server store log, sndSecure = " <> show sndSecure)
       [ SLTC
           { name = "create new queue",
-            saved = [CreateQueue $ testNewQueueRec sndSecure],
-            compacted = [CreateQueue $ testNewQueueRec sndSecure],
-            state = M.fromList [(EntityId "abcd", testNewQueueRec sndSecure)]
+            saved = [CreateQueue qr],
+            compacted = [CreateQueue qr],
+            state = M.fromList [(recipientId qr, qr)]
           },
         SLTC
           { name = "create and delete queue",
-            saved = [CreateQueue $ testNewQueueRec sndSecure, DeleteQueue $ EntityId "abcd"],
+            saved = [CreateQueue qr, DeleteQueue $ recipientId qr],
             compacted = [],
             state = M.fromList []
           },
         SLTC
           { name = "create queue and add notifier",
-            saved = [CreateQueue $ testNewQueueRec sndSecure, AddNotifier (EntityId "abcd") testNtfCreds],
-            compacted = [CreateQueue $ (testNewQueueRec sndSecure) {notifier = Just testNtfCreds}],
-            state = M.fromList [(EntityId "abcd", (testNewQueueRec sndSecure) {notifier = Just testNtfCreds})]
+            saved = [CreateQueue qr, AddNotifier (recipientId qr) testNtfCreds],
+            compacted = [CreateQueue $ qr {notifier = Just testNtfCreds}],
+            state = M.fromList [(recipientId qr, qr {notifier = Just testNtfCreds})]
           }
       ]
 
