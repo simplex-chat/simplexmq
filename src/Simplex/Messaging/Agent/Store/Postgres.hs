23ed1a1d0d3d10f79d4a0145171d8c5f3b52b74b
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2024-12-13 18:41:35 +0400

    methods

--- src/Simplex/Messaging/Agent/Store/Postgres.hs
+++ src/Simplex/Messaging/Agent/Store/Postgres.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Simplex.Messaging.Agent.Store.Postgres
   ( createDBStore,
     closeDBStore,
@@ -5,20 +7,30 @@ module Simplex.Messaging.Agent.Store.Postgres
   )
 where
 
+import Control.Exception (throwIO)
 import Data.Text (Text)
 import qualified Database.PostgreSQL.Simple as PSQL
 import Simplex.Messaging.Agent.Store.Postgres.Common
+import qualified Simplex.Messaging.Agent.Store.Postgres.DB as DB
 import Simplex.Messaging.Agent.Store.Shared (Migration (..), MigrationConfirmation (..), MigrationError (..))
+import Simplex.Messaging.Util (ifM)
+import UnliftIO.STM
 
 -- TODO [postgres] pass db name / ConnectInfo?
 createDBStore :: [Migration] -> MigrationConfirmation -> IO (Either MigrationError DBStore)
 createDBStore = undefined
 
+-- can share with SQLite
 closeDBStore :: DBStore -> IO ()
-closeDBStore = undefined
+closeDBStore st@DBStore {dbClosed} =
+  ifM (readTVarIO dbClosed) (putStrLn "closeDBStore: already closed") $
+    withConnection st $ \conn -> do
+      DB.close conn
+      atomically $ writeTVar dbClosed True
 
+-- TODO [postgres] not necessary for postgres (used for ExecAgentStoreSQL, ExecChatStoreSQL)
 execSQL :: PSQL.Connection -> Text -> IO [Text]
-execSQL = undefined
+execSQL _db _query = throwIO (userError "not implemented")
 
 -- createDatabaseIfNotExists :: ConnectInfo -> String -> IO ()
 -- createDatabaseIfNotExists defaultConnectInfo targetDbName = do
