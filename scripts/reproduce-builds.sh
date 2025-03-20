a1e329a5892c0ba99e554c844cc5b87a57b06de6
shum <shum@liber.li>
2025-03-20 13:27:47 +0000

    scripts: update build reproducibility script

--- scripts/reproduce-builds.sh
+++ scripts/reproduce-builds.sh
@@ -1,30 +1,64 @@
 #!/usr/bin/env sh
 set -eu
 
-tag="$1"
+TAG="$1"
 
-git clone https://github.com/simplex-chat/simplexmq && cd simplexmq
+tempdir="$(mktemp -d)"
+init_dir="$PWD"
 
-git checkout "$tag"
+mkdir -p "$init_dir/$TAG/from-source" "$init_dir/$TAG/prebuilt"
+
+git -C "$tempdir" clone https://github.com/simplex-chat/simplexmq.git &&\
+	cd "$tempdir/simplexmq" &&\
+	git checkout "$TAG"
 
 for os in 20.04 22.04 24.04; do
-	mkdir -p out-${os}-github;
+	os_url="$(printf '%s' "$os" | tr '.' '_')"
+	mkdir -p "$init_dir/cache/cabal/builder-${os}" "$init_dir/cache/dist-newstyle/builder-${os}"
+	chmod g+wX "$init_dir/cache"
 
-	docker build -f Dockerfile.build --build-arg TAG=${os} -t repro-${os} .
-	docker run -t -d --name builder-${os} repro-${os}
+		#--no-cache \
+	docker build \
+		-f "$tempdir/simplexmq/Dockerfile.build" \
+		--build-arg TAG=${os} \
+		-t repro-${os} \
+		.
+
+	docker run \
+		-t \
+		-d \
+		-v "$init_dir/cache/cabal/builder-${os}:/root/.cabal" \
+		-v "$init_dir/cache/dist-newstyle/builder-${os}:/dist-newstyle" \
+		-v "$tempdir/simplexmq:/project" \
+		--name builder-${os} \
+		repro-${os}
+	
 	
 	apps='smp-server xftp-server ntf-server xftp'
-	os_url="$(printf '%s' "$os" | tr '.' '_')"
-	
-	docker exec -t -e apps="$apps" builder-${os} sh -c 'cabal build && mkdir /out && for i in $apps; do bin=$(find /project/dist-newstyle -name "$i" -type f -executable); strip "$bin"; chmod +x "$bin"; mv "$bin" /out/; done'
+	docker exec \
+		-t \
+		-e apps="$apps" \
+		builder-${os} \
+		sh -c 'ln -s /dist-newstyle ./dist-newstyle && cabal update && cabal build && mkdir /out && for i in $apps; do bin=$(find /dist-newstyle -name "$i" -type f -executable); strip "$bin"; chmod +x "$bin"; mv "$bin" /out/; done'
 
-	docker cp builder-${os}:/out out-${os}
+	docker cp \
+		builder-${os}:/out \
+		out-${os}
 	
 	for app in $apps; do
-		curl -L "https://github.com/simplex-chat/simplexmq/releases/download/${tag}/${app}-ubuntu-${os_url}-x86-64" -o out-${os}-github/${app}
+		curl -L \
+			--output-dir "$init_dir/$TAG/prebuilt/" \
+			-O \
+		       	"https://github.com/simplex-chat/simplexmq/releases/download/${TAG}/${app}-ubuntu-${os_url}-x86-64"
+
+		mv "./out-${os}/$app" "$init_dir/$TAG/from-source/${app}-ubuntu-${os_url}-x86-64"
 	done
 
 	docker stop builder-${os}
 	docker rm builder-${os} 
 	docker image rm repro-${os}
 done
+
+# Cleanup
+cd "$init_dir"
+rm -rf "$tempdir"
