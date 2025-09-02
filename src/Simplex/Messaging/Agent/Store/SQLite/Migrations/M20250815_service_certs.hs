7fba70acc1e77df3613dff14ca389f1129a24c58
Evgeny Poberezkin <evgeny@poberezkin.com>
2025-09-02 22:59:03 +0100

    remove agent triggers

--- src/Simplex/Messaging/Agent/Store/SQLite/Migrations/M20250815_service_certs.hs
+++ src/Simplex/Messaging/Agent/Store/SQLite/Migrations/M20250815_service_certs.hs
@@ -25,60 +25,11 @@ CREATE UNIQUE INDEX idx_server_certs_user_id_host_port ON client_services(user_i
 CREATE INDEX idx_server_certs_host_port ON client_services(host, port);
 
 ALTER TABLE rcv_queues ADD COLUMN rcv_service_assoc INTEGER NOT NULL DEFAULT 0;
-
-CREATE TRIGGER tr_rcv_queue_insert
-AFTER INSERT ON rcv_queues
-FOR EACH ROW
-WHEN NEW.rcv_service_assoc != 0 AND NEW.deleted = 0
-BEGIN
-  UPDATE client_services
-  SET service_queue_count = service_queue_count + 1,
-      service_queue_ids_hash = chat_xor_combine(service_queue_ids_hash, chat_md5hash(NEW.rcv_id))
-  WHERE user_id = NEW.user_id AND host = NEW.host AND port = NEW.port;
-END;
-
-CREATE TRIGGER tr_rcv_queue_delete
-AFTER DELETE ON rcv_queues
-FOR EACH ROW
-WHEN OLD.rcv_service_assoc != 0 AND OLD.deleted = 0
-BEGIN
-  UPDATE client_services
-  SET service_queue_count = service_queue_count - 1,
-      service_queue_ids_hash = chat_xor_combine(service_queue_ids_hash, chat_md5hash(OLD.rcv_id))
-  WHERE user_id = OLD.user_id AND host = OLD.host AND port = OLD.port;
-END;
-
-CREATE TRIGGER tr_rcv_queue_update_remove
-AFTER UPDATE ON rcv_queues
-FOR EACH ROW
-WHEN OLD.rcv_service_assoc != 0 AND OLD.deleted = 0 AND NOT (NEW.rcv_service_assoc != 0 AND NEW.deleted = 0)
-BEGIN
-  UPDATE client_services
-  SET service_queue_count = service_queue_count - 1,
-      service_queue_ids_hash = chat_xor_combine(service_queue_ids_hash, chat_md5hash(OLD.rcv_id))
-  WHERE user_id = OLD.user_id AND host = OLD.host AND port = OLD.port;
-END;
-
-CREATE TRIGGER tr_rcv_queue_update_add
-AFTER UPDATE ON rcv_queues
-FOR EACH ROW
-WHEN NEW.rcv_service_assoc != 0 AND NEW.deleted = 0 AND NOT (OLD.rcv_service_assoc != 0 AND OLD.deleted = 0)
-BEGIN
-  UPDATE client_services
-  SET service_queue_count = service_queue_count + 1,
-      service_queue_ids_hash = chat_xor_combine(service_queue_ids_hash, chat_md5hash(NEW.rcv_id))
-  WHERE user_id = NEW.user_id AND host = NEW.host AND port = NEW.port;
-END;
   |]
 
 down_m20250815_service_certs :: Query
 down_m20250815_service_certs =
   [sql|
-DROP TRIGGER tr_rcv_queue_insert;
-DROP TRIGGER tr_rcv_queue_delete;
-DROP TRIGGER tr_rcv_queue_update_remove;
-DROP TRIGGER tr_rcv_queue_update_add;
-
 ALTER TABLE rcv_queues DROP COLUMN rcv_service_assoc;
 
 DROP INDEX idx_server_certs_host_port;
