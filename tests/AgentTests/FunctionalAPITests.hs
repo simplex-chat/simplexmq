9c07ddff3cbd2302f0f02c5506db89e261bca1e0
spaced4ndy <8711996+spaced4ndy@users.noreply.github.com>
2026-03-30 09:48:31 +0000

    agent: allow to use existing connId for getConnShortLinkAsync (#1752)

--- tests/AgentTests/FunctionalAPITests.hs
+++ tests/AgentTests/FunctionalAPITests.hs
@@ -2736,7 +2736,7 @@ testGetConnShortLinkAsync ps = withAgentClients2 $ \alice bob ->
         newLinkData = UserContactLinkData userCtData
     (_, (CCLink qInfo (Just shortLink), _)) <- A.createConnection alice NRMInteractive 1 True True SCMContact (Just newLinkData) Nothing IKPQOn SMSubscribe
     -- get link data async - creates new connection for bob
-    newId <- getConnShortLinkAsync bob 1 "1" shortLink
+    newId <- getConnShortLinkAsync bob 1 "1" Nothing shortLink
     ("1", newId', LDATA FixedLinkData {linkConnReq = qInfo'} (ContactLinkData _ userCtData')) <- get bob
     liftIO $ newId' `shouldBe` newId
     liftIO $ qInfo' `shouldBe` qInfo
