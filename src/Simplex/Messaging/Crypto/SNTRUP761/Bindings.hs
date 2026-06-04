b846a0b8350a5287357b8f9afe9ab5af4ebe0ae8
Paul Bottinelli <paul.bottinelli@trailofbits.com>
2026-06-04 11:32:09 -0400

    Validate SNTRUP761 KEM input lengths

--- src/Simplex/Messaging/Crypto/SNTRUP761/Bindings.hs
+++ src/Simplex/Messaging/Crypto/SNTRUP761/Bindings.hs
@@ -13,6 +13,7 @@ module Simplex.Messaging.Crypto.SNTRUP761.Bindings
   ) where
 
 import Control.Concurrent.STM
+import Control.Exception (throwIO)
 import Crypto.Random (ChaChaDRG)
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import Data.Bifunctor (bimap)
@@ -55,7 +56,8 @@ sntrup761Keypair drg =
       )
 
 sntrup761Enc :: TVar ChaChaDRG -> KEMPublicKey -> IO (KEMCiphertext, KEMSharedKey)
-sntrup761Enc drg (KEMPublicKey pk) =
+sntrup761Enc drg (KEMPublicKey pk) = do
+  requireByteArrayLength "SNTRUP761 public key" c_SNTRUP761_PUBLICKEY_SIZE pk
   BA.withByteArray pk $ \pkPtr ->
     bimap KEMCiphertext KEMSharedKey
       <$> BA.allocRet
@@ -66,31 +68,63 @@ sntrup761Enc drg (KEMPublicKey pk) =
         )
 
 sntrup761Dec :: KEMCiphertext -> KEMSecretKey -> IO KEMSharedKey
-sntrup761Dec (KEMCiphertext c) (KEMSecretKey sk) =
+sntrup761Dec (KEMCiphertext c) (KEMSecretKey sk) = do
+  requireByteArrayLength "SNTRUP761 ciphertext" c_SNTRUP761_CIPHERTEXT_SIZE c
+  requireByteArrayLength "SNTRUP761 secret key" c_SNTRUP761_SECRETKEY_SIZE sk
   BA.withByteArray sk $ \skPtr ->
     BA.withByteArray c $ \cPtr ->
       KEMSharedKey
         <$> BA.alloc c_SNTRUP761_SIZE (\kPtr -> c_sntrup761_dec kPtr cPtr skPtr)
 
+requireByteArrayLength :: BA.ByteArrayAccess bytes => String -> Int -> bytes -> IO ()
+requireByteArrayLength valueName expected bytes =
+  either (throwIO . userError) (const $ pure ()) $
+    validateByteArrayLength valueName expected bytes
+
+validateByteArrayLength :: BA.ByteArrayAccess bytes => String -> Int -> bytes -> Either String bytes
+validateByteArrayLength valueName expected bytes
+  | actual == expected = Right bytes
+  | otherwise = Left $ valueName <> " must be " <> show expected <> " bytes, got " <> show actual
+  where
+    actual = BA.length bytes
+
+parseKEMPublicKey :: ByteString -> Either String KEMPublicKey
+parseKEMPublicKey =
+  fmap KEMPublicKey . validateByteArrayLength "SNTRUP761 public key" c_SNTRUP761_PUBLICKEY_SIZE
+
+parseKEMSecretKey :: ScrubbedBytes -> Either String KEMSecretKey
+parseKEMSecretKey =
+  fmap KEMSecretKey . validateByteArrayLength "SNTRUP761 secret key" c_SNTRUP761_SECRETKEY_SIZE
+
+parseKEMCiphertext :: ByteString -> Either String KEMCiphertext
+parseKEMCiphertext =
+  fmap KEMCiphertext . validateByteArrayLength "SNTRUP761 ciphertext" c_SNTRUP761_CIPHERTEXT_SIZE
+
 instance Encoding KEMSecretKey where
   smpEncode (KEMSecretKey c) = smpEncode . Large $ BA.convert c
-  smpP = KEMSecretKey . BA.convert . unLarge <$> smpP
+  smpP = do
+    Large bytes <- smpP
+    either fail pure $ parseKEMSecretKey (BA.convert bytes)
 
 instance StrEncoding KEMSecretKey where
   strEncode (KEMSecretKey pk) = strEncode (BA.convert pk :: ByteString)
-  strP = KEMSecretKey . BA.convert <$> strP @ByteString
+  strP = either fail pure . parseKEMSecretKey . BA.convert =<< strP @ByteString
 
 instance Encoding KEMPublicKey where
   smpEncode (KEMPublicKey pk) = smpEncode . Large $ BA.convert pk
-  smpP = KEMPublicKey . BA.convert . unLarge <$> smpP
+  smpP = do
+    Large bytes <- smpP
+    either fail pure $ parseKEMPublicKey bytes
 
 instance StrEncoding KEMPublicKey where
   strEncode (KEMPublicKey pk) = strEncode (BA.convert pk :: ByteString)
-  strP = KEMPublicKey . BA.convert <$> strP @ByteString
+  strP = either fail pure . parseKEMPublicKey =<< strP @ByteString
 
 instance Encoding KEMCiphertext where
   smpEncode (KEMCiphertext c) = smpEncode . Large $ BA.convert c
-  smpP = KEMCiphertext . BA.convert . unLarge <$> smpP
+  smpP = do
+    Large bytes <- smpP
+    either fail pure $ parseKEMCiphertext bytes
 
 instance Encoding KEMSharedKey where
   smpEncode (KEMSharedKey c) = smpEncode (BA.convert c :: ByteString)
@@ -98,7 +132,7 @@ instance Encoding KEMSharedKey where
 
 instance StrEncoding KEMCiphertext where
   strEncode (KEMCiphertext pk) = strEncode (BA.convert pk :: ByteString)
-  strP = KEMCiphertext . BA.convert <$> strP @ByteString
+  strP = either fail pure . parseKEMCiphertext =<< strP @ByteString
 
 instance StrEncoding KEMSharedKey where
   strEncode (KEMSharedKey pk) = strEncode (BA.convert pk :: ByteString)
